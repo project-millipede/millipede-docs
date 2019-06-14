95cc37a50abb2d03176cafc0c660d2f53b488812
Gritsch Markus <markus.gritsch@millipede.me>
2019-06-14 22:20:53 +0200

    General application frame

---
+++ docs/src/modules/components/PageTitle.tsx
@@ -0,0 +1,24 @@
+import { useHoux } from 'houx';
+
+import { RootState } from '../redux/reducers';
+import { pageToTitleI18n } from '../utils/helpers';
+
+interface PageTitleProps {
+  children: (title: string) => JSX.Element;
+  // t?: (title: string) => string;
+}
+
+function PageTitle(props: PageTitleProps) {
+  const { state }: { state: RootState } = useHoux();
+
+  if (!state.navigation) {
+    throw new Error("Missing activePage.");
+  }
+
+  // const title = pageToTitleI18n(state.navigation.activePage, props.t);
+  const title = pageToTitleI18n(state.navigation.activePage, undefined);
+
+  return props.children(title);
+}
+
+export default PageTitle;
