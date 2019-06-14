b1ada033a58215c94dff427903787df871b97e26
Gritsch Markus <markus.gritsch@millipede.me>
2019-06-14 22:07:39 +0200

    Add second level components related to material-ui

---
+++ docs/src/modules/components/AppWrapper.tsx
@@ -0,0 +1,53 @@
+import { createGenerateClassName, jssPreset, StylesProvider } from "@material-ui/styles";
+import { useHoux } from "houx";
+import { create } from "jss";
+import withRouter, { WithRouterProps } from "next/dist/client/with-router";
+import React, { useEffect } from "react";
+
+import { Provider as ThemeProvider } from "./ThemeContext";
+
+import pages from "../../pages";
+import { NavigationActions } from "../redux/features/actionType";
+import { changeNavigation } from "../redux/features/navigation/actions";
+import { determineCurrenPathname, findActivePage } from "../utils/router";
+
+interface OProps extends React.Props<any> {}
+
+type Props = OProps & WithRouterProps;
+
+// Configure JSS
+const jss = create({
+  plugins: [...jssPreset().plugins]
+});
+
+const generateClassName = createGenerateClassName();
+
+const AppWrapper = ({ children, router }: Props) => {
+  // Remove the server-side injected CSS.
+  // const jssStyles = document.querySelector("#jss-server-side");
+  // if (jssStyles) {
+  //   jssStyles.parentNode.removeChild(jssStyles);
+  // }
+
+  const pathname = determineCurrenPathname(router.pathname);
+  const activePage = findActivePage(pages, pathname);
+  const { dispatch }: { dispatch: React.Dispatch<NavigationActions> } = useHoux();
+  useEffect(() => {
+    dispatch(changeNavigation(activePage));
+  }, []);
+
+  // return (
+  //   <StylesProvider generateClassName={generateClassName} jss={jss}>
+  //     {children}
+  //   </StylesProvider>
+  // );
+
+  return (
+    <StylesProvider jss={jss}>
+      <ThemeProvider>{children}</ThemeProvider>
+      {/* <SideEffects /> */}
+    </StylesProvider>
+  );
+};
+
+export default withRouter(AppWrapper);
