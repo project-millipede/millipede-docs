cbbf9e4ab0a6781fee232159007bb530584887f7
Gritsch Markus <markus.gritsch@millipede.me>
2019-06-15 02:23:40 +0200

    First content page -> common/dataflow/comparison

---
+++ src/assets/common/dataFlow/comparison/Step2.tsx
@@ -0,0 +1,151 @@
+import React from 'react';
+
+const SvgDataFlowStep1Scaled = props => (
+  <svg width={800} height={700} {...props}>
+    <g fill="none" fillRule="evenodd">
+      <path stroke="#000" fill="#D8D8D8" d="M200.5 75.5h99v49h-99z" />
+      <text fontFamily="Roboto" fontSize={18} fontWeight={400} fill="#000">
+        <tspan x={208.34} y={94}>
+          {"Determine"}
+        </tspan>
+        <tspan x={219.339} y={118}>
+          {"content"}
+        </tspan>
+      </text>
+      <path stroke="#000" fill="#D8D8D8" d="M350.5 75.5h99v49h-99z" />
+      <text fontFamily="Roboto" fontSize={18} fontWeight={400} fill="#000">
+        <tspan x={368.434} y={94}>
+          {"Receive"}
+        </tspan>
+        <tspan x={369.339} y={118}>
+          {"content"}
+        </tspan>
+      </text>
+      <path stroke="#000" fill="#D8D8D8" d="M500.5 75.5h99v49h-99z" />
+      <text fontFamily="Roboto" fontSize={18} fontWeight={400} fill="#000">
+        <tspan x={524.195} y={94}>
+          {"Merge"}
+        </tspan>
+        <tspan x={519.339} y={118}>
+          {"content"}
+        </tspan>
+      </text>
+      <path
+        d="M249.5 125v50M49.5 50v50M400.5 125v50M550.5 125v50M750.5 50v50"
+        stroke="#000"
+        strokeLinecap="square"
+      />
+      <path d="M525.5 349.5h49v-25h50v-149h-449v149h50v25h49v-25h251v25z" stroke="#000" />
+      <path
+        stroke="#000"
+        d="M225.5 225.5h49v49h-49zM375.5 225.5h49v49h-49zM525.5 225.5h49v49h-49zM225.5 425.5h49v49h-49zM525.5 425.5h49v49h-49z"
+      />
+      <path
+        d="M361 250.5h-87v-2h87v-6l14 7-14 7v-6zM511 250.5h-87v-2h87v-6l14 7-14 7v-6z"
+        fill="#00A"
+        fillRule="nonzero"
+      />
+      <path d="M575.5 375.5v25h-51v-25h-249v25h-51v-25h-49v149h449v-149h-49z" stroke="#000" />
+      <path stroke="#000" fill="#D8D8D8" d="M500.5 575.5h99v49h-99z" />
+      <text fontFamily="Roboto" fontSize={18} fontWeight={400} fill="#000">
+        <tspan x={520.561} y={594}>
+          {"Display"}
+        </tspan>
+        <tspan x={519.339} y={618}>
+          {"content"}
+        </tspan>
+      </text>
+      <path stroke="#000" fill="#D8D8D8" d="M.5 650.5h99v49H.5z" />
+      <text fontFamily="Roboto" fontSize={18} fontWeight={400} fill="#000">
+        <tspan x={18.434} y={669}>
+          {"Receive"}
+        </tspan>
+        <tspan x={19.339} y={693}>
+          {"content"}
+        </tspan>
+      </text>
+      <path d="M49.5 600v50" stroke="#000" strokeLinecap="square" />
+      <path stroke="#000" fill="#D8D8D8" d="M.5 650.5h99v49H.5z" />
+      <text fontFamily="Roboto" fontSize={18} fontWeight={400} fill="#000">
+        <tspan x={8.34} y={669}>
+          {"Determine"}
+        </tspan>
+        <tspan x={19.339} y={693}>
+          {"content"}
+        </tspan>
+      </text>
+      <path d="M49.5 600v50M550.5 525v50" stroke="#000" strokeLinecap="square" />
+      <path stroke="#000" fill="#D8D8D8" d="M200.5 575.5h99v49h-99z" />
+      <text
+        stroke="#979797"
+        strokeWidth={0.5}
+        fontFamily="Roboto"
+        fontSize={18}
+        fontWeight={400}
+        fill="#000"
+      >
+        <tspan x={218.434} y={594}>
+          {"Receive"}
+        </tspan>
+        <tspan x={219.339} y={618}>
+          {"content"}
+        </tspan>
+      </text>
+      <path d="M249.5 525v50" stroke="#000" strokeLinecap="square" />
+      <path stroke="#000" fill="#D8D8D8" d="M700.5 650.5h99v49h-99z" />
+      <text fontFamily="Roboto" fontSize={18} fontWeight={400} fill="#000">
+        <tspan x={702.724} y={669}>
+          {"Interpretate"}
+        </tspan>
+        <tspan x={719.339} y={693}>
+          {"content"}
+        </tspan>
+      </text>
+      <path d="M750.5 600v50" stroke="#000" strokeLinecap="square" />
+      <path stroke="#000" d="M25.5 100.5h49v499h-49z" />
+      <path
+        d="M250.5 289v137h-2V289h-6l7-14 7 14h-6zM549.5 411V274h2v137h6l-7 14-7-14h6z"
+        fill="#00A"
+        fillRule="nonzero"
+      />
+      <path stroke="#000" fill="#D8D8D8" d="M37.5 375.5h24v149h-24zM738.5 375.5h24v149h-24z" />
+      <path
+        d="M225 450.5l-14 7v-14l14 7zm-164 1v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7z"
+        fill="#000"
+        fillRule="nonzero"
+      />
+      <path stroke="#000" d="M725.5 100.5h49v499h-49z" />
+      <path
+        d="M711 451.5h-7v-2h7v-6l14 7-14 7v-6zm-137 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7zm10 0v-2h7v2h-7z"
+        fill="#000"
+        fillRule="nonzero"
+      />
+      <path stroke="#000" fill="#A00" d="M.5.5h99v24H.5z" />
+      <text fontFamily="Roboto" fontSize={18} fontWeight={400} fill="#000">
+        <tspan x={14.576} y={19}>
+          {"Operator"}
+        </tspan>
+      </text>
+      <path stroke="#000" fill="#D8D8D8" d="M.5 25.5h99v24H.5z" />
+      <text fontFamily="Roboto" fontSize={18} fontWeight={400} fill="#000">
+        <tspan x={4.78} y={44}>
+          {"Remote DC"}
+        </tspan>
+      </text>
+      <path stroke="#000" fill="#0A0" d="M700.5.5h99v24h-99z" />
+      <text fontFamily="Roboto" fontSize={18} fontWeight={400} fill="#000">
+        <tspan x={731.495} y={19}>
+          {"User"}
+        </tspan>
+      </text>
+      <path stroke="#000" fill="#D8D8D8" d="M700.5 25.5h99v24h-99z" />
+      <text fontFamily="Roboto" fontSize={18} fontWeight={400} fill="#000">
+        <tspan x={703.651} y={44}>
+          {"Own device"}
+        </tspan>
+      </text>
+    </g>
+  </svg>
+);
+
+export default SvgDataFlowStep1Scaled;
---
+++ src/components/stepper/DotsMobileStepper.tsx
@@ -0,0 +1,59 @@
+import Button from '@material-ui/core/Button';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from '@material-ui/styles';
+import React, { useEffect, useState } from 'react';
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1
+  }
+});
+
+type StepperProps = {
+  steps: number;
+  currentStep: (step: number) => void;
+};
+
+function DotsMobileStepper(props: StepperProps) {
+  const classes = useStyles();
+  // const theme = useTheme();
+  const [activeStep, setActiveStep] = useState(0);
+
+  useEffect(() => {
+    props.currentStep(activeStep);
+  });
+
+  function handleNext() {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  }
+
+  function handleBack() {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  }
+
+  return (
+    <MobileStepper
+      variant="dots"
+      steps={props.steps}
+      position="static"
+      activeStep={activeStep}
+      className={classes.root}
+      nextButton={
+        <Button size="small" onClick={handleNext} disabled={activeStep === props.steps - 1}>
+          Next
+          <KeyboardArrowRight />
+        </Button>
+      }
+      backButton={
+        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+          <KeyboardArrowLeft />
+          Back
+        </Button>
+      }
+    />
+  );
+}
+
+export default DotsMobileStepper;
