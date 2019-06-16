5b3d6f3a54af17b7ebbbea1b572a611a7ad86b24
Gritsch Markus <markus.gritsch@millipede.me>
2019-06-16 12:34:47 +0200

    Exclude node_modules in rules section of webpack

--- webpack/webpack.dev.conf.ts
+++ webpack/webpack.dev.conf.ts
@@ -4,29 +4,30 @@ import webpack from 'webpack';
 const webpackDevConfig: webpack.Configuration = {
   // Fixes npm packages that depend on `fs` module
   node: {
-    fs: "empty"
+    fs: 'empty'
   },
 
   // The context is two levels out, because next does currently not support
   // configurations (next.config) in typescript
-  context: path.resolve(__dirname, "../../"),
+  context: path.resolve(__dirname, '../../'),
 
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
   },
 
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: "ts-loader" },
-
+      { test: /\.tsx?$/, loader: 'ts-loader' },
       {
         test: /\.mdx$/,
-        use: ["babel-loader", path.join(__dirname, "./loader/mdx-custom-loader")]
+        exclude: /node_modules/,
+        use: ['babel-loader', path.join(__dirname, './loader/mdx-custom-loader')]
       },
       {
         test: /\.md$/,
-        use: "raw-loader"
+        exclude: /node_modules/,
+        use: 'raw-loader'
       }
     ]
   },
