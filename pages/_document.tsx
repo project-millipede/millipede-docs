12438ee6452c4e78f31021ace13ef2fe448a90af
Gritsch Markus <markus.gritsch@millipede.me>
2019-06-18 08:10:33 +0200

    Improve _app _document, both files are most important for propper ssr

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,70 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/styles';
+import NextDocument, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
-class MillipedeDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+class MillipedeDocument extends NextDocument {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    // Resolution order
+    //
+    // On the server:
+    // 1. app.getInitialProps
+    // 2. page.getInitialProps
+    // 3. document.getInitialProps
+    // 4. app.render
+    // 5. page.render
+    // 6. document.render
+    //
+    // On the server with error:
+    // 1. document.getInitialProps
+    // 2. app.render
+    // 3. page.render
+    // 4. document.render
+    //
+    // On the client
+    // 1. app.getInitialProps
+    // 2. page.getInitialProps
+    // 3. app.render
+    // 4. page.render
+
+    // Render app and page and get the context of the page with collected side effects.
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheets.collect(<App {...props} />)
+      });
+
+    const initialProps = await NextDocument.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
+      styles: (
+        <React.Fragment>
+          {initialProps.styles}
+          {sheets.getStyleElement()}
+        </React.Fragment>
+      ) as any
+    };
   }
 
   render() {
     return (
-      <Html>
+      <Html lang='en'>
         <Head>
-          <style>{`body { margin: 0 } /* custom! */`}</style>
-          <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
+          <meta charSet='utf-8' />
+          {/* Use minimum-scale=1 to enable GPU rasterization */}
+          <meta
+            name='viewport'
+            content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no'
+          />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+          />
         </Head>
-        <body className="custom_class">
+        <body>
           <Main />
           <NextScript />
         </body>
