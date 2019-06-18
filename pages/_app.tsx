79a78ae79d8b15eef06f6eb19d11effed8190bf6
Gritsch Markus <markus.gritsch@millipede.me>
2019-06-18 08:10:33 +0200

    Improve _app _document, both files are most important for propper ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,39 @@ import React from 'react';
 import AppWrapper from '../docs/src/modules/components/AppWrapper';
 import reducers from '../docs/src/modules/redux/reducers';
 
+const USE_STRICT_MODE = false;
+const ReactMode = USE_STRICT_MODE ? React.StrictMode : React.Fragment;
+
 class MillipedeApp extends App {
+  componentDidMount() {
+    // TODO - determine purpose
+    // Remove the server-side injected CSS.
+    // const jssStyles = document.querySelector('#jss-server-side');
+    // if (jssStyles) {
+    //   jssStyles.parentNode.removeChild(jssStyles);
+    // }
+  }
+
   render() {
+    const fonts = ['https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'];
     const { Component, pageProps } = this.props;
+
     return (
-      <Container>
-        <HouxProvider reducers={reducers} logDispatchedActions>
-          <AppWrapper>
-            <Component {...pageProps} />
-          </AppWrapper>
-        </HouxProvider>
-      </Container>
+      <ReactMode>
+        {/* TODO - determine purpose */}
+        {/* <NextHead>
+          {fonts.map(font => (
+            <link rel='stylesheet' href={font} key={font} />
+          ))}
+        </NextHead> */}
+        <Container>
+          <HouxProvider reducers={reducers} logDispatchedActions>
+            <AppWrapper>
+              <Component {...pageProps} />
+            </AppWrapper>
+          </HouxProvider>
+        </Container>
+      </ReactMode>
     );
   }
 }
