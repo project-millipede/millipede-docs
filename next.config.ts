40d11da399cd92c850b56839883a178d5e4385b4
Gritsch Markus <markus.gritsch@millipede.me>
2019-06-20 00:41:52 +0200

    Borrow a snippet to ensure that dependencies specified in _app are not duplicated in other pages.

--- next.config.ts
+++ next.config.ts
@@ -9,12 +9,40 @@ const composeFn = compose();
 
 export default composeFn({
   webpack(config, options) {
+    /**
+     * The following snippet was borrowed from https://github.com/zeit/next.js/issues/5923
+     */
+
+    const { isServer } = options;
+
+    if (!isServer) {
+      const cacheGroups = config.optimization.splitChunks.cacheGroups;
+
+      delete cacheGroups.react;
+
+      cacheGroups.default = false;
+
+      cacheGroups.vendors = {
+        name: 'vendors',
+        test: /[\\/](node_modules|packages)[\\/]/,
+        enforce: true,
+        priority: 20
+      };
+
+      cacheGroups.commons = {
+        name: 'commons',
+        minChunks: 2,
+        priority: 10
+      };
+    }
+
     return merge(config, webpackConfig);
 
     // // Do not run type checking twice:
     // if (options.isServer && options.dev) config.plugins.push(new ForkTsCheckerWebpackPlugin());
     // return config;
   },
-  target: "serverless",
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"]
+
+  target: 'serverless',
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx']
 });
