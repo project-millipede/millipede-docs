85f6d88e228ecd0204d4ed80f43320f354606be5
Gritsch Markus <markus.gritsch@millipede.me>
2019-06-20 00:40:16 +0200

    Remove duplicated code

--- docs/src/modules/components/AppFrame.tsx
+++ docs/src/modules/components/AppFrame.tsx
@@ -1,10 +1,5 @@
-import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import IconButton from '@material-ui/core/IconButton';
 import { Theme } from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import MenuIcon from '@material-ui/icons/Menu';
 import { createStyles, makeStyles } from '@material-ui/styles';
 import React, { useState } from 'react';
 
@@ -15,71 +10,71 @@ import PageTitle from './PageTitle';
 
 export const languages = [
   {
-    code: "en",
-    text: "🇺🇸 English"
+    code: 'en',
+    text: '🇺🇸 English'
   },
   {
-    code: "de",
-    text: "🇩🇪 Deutsch"
+    code: 'de',
+    text: '🇩🇪 Deutsch'
   }
 ];
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      display: "flex"
+      display: 'flex'
     },
     grow: {
-      flex: "1 1 auto"
+      flex: '1 1 auto'
     },
     title: {
       marginLeft: theme.spacing(2),
-      flex: "0 1 auto"
+      flex: '0 1 auto'
     },
     skipNav: {
-      position: "fixed",
+      position: 'fixed',
       padding: theme.spacing(1),
       backgroundColor: theme.palette.background.paper,
-      transition: theme.transitions.create("top", {
+      transition: theme.transitions.create('top', {
         easing: theme.transitions.easing.easeIn,
         duration: theme.transitions.duration.leavingScreen
       }),
       left: theme.spacing(2),
       top: theme.spacing(-10),
       zIndex: theme.zIndex.tooltip + 1,
-      "&:focus": {
+      '&:focus': {
         top: theme.spacing(2),
-        transition: theme.transitions.create("top", {
+        transition: theme.transitions.create('top', {
           easing: theme.transitions.easing.easeOut,
           duration: theme.transitions.duration.enteringScreen
         })
       },
-      "@media print": {
-        display: "none"
+      '@media print': {
+        display: 'none'
       }
     },
     appBar: {
-      transition: theme.transitions.create("width"),
-      "@media print": {
-        position: "absolute"
+      transition: theme.transitions.create('width'),
+      '@media print': {
+        position: 'absolute'
       }
     },
     appBarHome: {
-      boxShadow: "none"
+      boxShadow: 'none'
     },
     appBarShift: {
-      [theme.breakpoints.up("lg")]: {
-        width: "calc(100% - 240px)"
+      [theme.breakpoints.up('lg')]: {
+        width: 'calc(100% - 240px)'
       }
     },
     navIconHide: {
-      [theme.breakpoints.up("lg")]: {
-        display: "none"
+      [theme.breakpoints.up('lg')]: {
+        display: 'none'
       }
     },
-    "@global": {
-      "#main-content": {
-        outline: "none"
+    '@global': {
+      '#main-content': {
+        outline: 'none'
       }
     }
   })
@@ -127,7 +122,7 @@ const AppFrame = ({ children }: Props) => {
     // t={state.option.t}
     >
       {title => {
-        let navIconClassName = "";
+        let navIconClassName = '';
         let appBarClassName = classes.appBar;
 
         if (title === null) {
@@ -142,25 +137,6 @@ const AppFrame = ({ children }: Props) => {
           <div className={classes.root}>
             {/* <NProgressBar /> */}
             <CssBaseline />
-            <AppBar className={appBarClassName}>
-              <Toolbar>
-                <IconButton
-                  edge="start"
-                  color="inherit"
-                  aria-label="Open drawer"
-                  onClick={handleDrawerOpen}
-                  className={navIconClassName}
-                >
-                  <MenuIcon />
-                </IconButton>
-                {title !== null && (
-                  <Typography className={classes.title} variant="h6" noWrap>
-                    {title}
-                  </Typography>
-                )}
-                <div className={classes.grow} />
-              </Toolbar>
-            </AppBar>
             <MiniDrawer onClose={handleDrawerClose} onOpen={handleDrawerOpen} />
             {children}
           </div>
