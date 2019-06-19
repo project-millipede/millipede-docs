770af7f98f800d796c229426e63167aa213ad61b
Gritsch Markus <markus.gritsch@millipede.me>
2019-06-20 00:39:31 +0200

    Integrate breadcrumbs in the application

--- docs/src/modules/components/md/MDDocs.tsx
+++ docs/src/modules/components/md/MDDocs.tsx
@@ -1,32 +1,11 @@
-import { createStyles, Theme } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
 import React, { useCallback, useEffect, useState } from 'react';
 import { scroller } from 'react-scroll';
 
 import AppContent from '../AppContent';
 import AppFrame from '../AppFrame';
 import AppTableOfContents from '../AppTableOfContents';
+import Breadcrumbs from '../common/breadcrumbs';
 import MarkdownElement from './MarkdownElement';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      marginBottom: 100
-    },
-    header: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "flex-end"
-    },
-    markdownElement: {
-      marginTop: theme.spacing(2),
-      marginBottom: theme.spacing(2),
-      padding: theme.spacing(0, 1)
-    }
-  })
-);
-
-const renderComponent = (Comp: React.FunctionComponent) => <Comp />;
 
 interface MarkdownDocsProps {
   content: string;
@@ -37,30 +16,21 @@ const scrollToLink = (href: string) => {
     duration: 600,
     offset: -85,
     delay: 0,
-    smooth: "ease",
-    containerId: "main-content"
+    smooth: 'ease',
+    containerId: 'main-content'
   });
 };
 
 export const MDDocs = (props: MarkdownDocsProps) => {
   const { content } = props;
-
-  const classes = useStyles();
-
   return (
-    // <MarkdownDocsContents
-    //   markdown={content}
-    //   // markdownLocation={markdownLocationProp}
-    // >
-    //   {({ contents, markdownLocation }) => (
     <AppFrame>
       <AppTableOfContents content={content} scrollToLink={scrollToLink} />
-      <AppContent className={classes.root}>
+      <AppContent>
+        <Breadcrumbs />
         <MarkdownElement content={content} />
       </AppContent>
     </AppFrame>
-    //   )}
-    // </MarkdownDocsContents>
   );
 };
 
