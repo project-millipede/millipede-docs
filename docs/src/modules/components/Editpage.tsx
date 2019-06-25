b022e89a62a6e63220e5b156bf019f9fe5ed457b
Gritsch Markus <markus.gritsch@millipede.me>
2019-06-25 23:16:17 +0200

    Add new component allowing users to edit existing pages, etither through github or crowdin.

---
+++ docs/src/modules/components/Editpage.tsx
@@ -0,0 +1,44 @@
+import Button from '@material-ui/core/Button';
+import { useHoux } from 'houx';
+import React from 'react';
+
+import { RootState } from '../redux/reducers';
+
+const LOCALES = { zh: 'zh-CN', pt: 'pt-BR', es: 'es-ES' };
+const CROWDIN_ROOT_URL = 'https://crowdin.com/project/project-millipede';
+const SOURCE_CODE_ROOT_URL = 'https://github.com/gurkerl83/millipede-docs/edit/master';
+
+export interface EditPageProps {
+  markdownLocation: string;
+  sourceCodeRootUrl: string;
+}
+
+export const EditPage = ({ markdownLocation }: EditPageProps) => {
+  const { state }: { state: RootState } = useHoux();
+  const { userLanguage } = state.language;
+
+  //   const { t } = props;
+  const crowdInLocale = LOCALES[userLanguage] || userLanguage;
+  const crowdInPath = markdownLocation.substring(0, markdownLocation.lastIndexOf('/'));
+
+  return (
+    <Button
+      component='a'
+      href={
+        userLanguage === 'de'
+          ? `${SOURCE_CODE_ROOT_URL}${markdownLocation}`
+          : `${CROWDIN_ROOT_URL}${crowdInLocale}#/staging${crowdInPath}`
+      }
+      target='_blank'
+      rel='noopener'
+      data-ga-event-category={userLanguage === 'de' ? undefined : 'l10n'}
+      data-ga-event-action={userLanguage === 'de' ? undefined : 'edit-button'}
+      data-ga-event-label={userLanguage === 'de' ? undefined : userLanguage}
+    >
+      {/* {t('editPage')} */}
+      {'Edit Page'}
+    </Button>
+  );
+};
+
+export default EditPage;
