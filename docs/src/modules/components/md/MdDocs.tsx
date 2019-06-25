706910c1fea55b5a8996f7898ede06614c7a30dd
Gritsch Markus <markus.gritsch@millipede.me>
2019-06-25 23:18:55 +0200

    Integrate editPage in markdown based pages.

--- docs/src/modules/components/md/MdDocs.tsx
+++ docs/src/modules/components/md/MdDocs.tsx
@@ -1,25 +1,61 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
 import React from 'react';
 
 import AppContent from '../AppContent';
 import AppFrame from '../AppFrame';
 import AppTableOfContents from '../AppTableOfContents';
 import Breadcrumbs from '../common/breadcrumbs';
+import EditPage from '../Editpage';
+import MarkdownDocsContents from '../MarkdownDocsContents';
 import MdElement from './MdElement';
 
 interface MarkdownDocsProps {
   content: string;
+  markdownLocation?: string;
 }
 
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    header: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      flexDirection: 'row'
+    },
+    headerSpace: {
+      flexGrow: 1
+    },
+    headerItem: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      flexDirection: 'column'
+    }
+  })
+);
+
+const SOURCE_CODE_ROOT_URL = 'https://github.com/gurkerl83/millipede-docs/blob/master/docs/src';
+
 export const MdDocs = (props: MarkdownDocsProps) => {
-  const { content } = props;
+  const classes = useStyles({});
+  const { content, markdownLocation: markdownLocationProp } = props;
   return (
-    <AppFrame>
-      <AppTableOfContents content={content} />
-      <AppContent>
-        <Breadcrumbs />
-        <MdElement content={content} />
-      </AppContent>
-    </AppFrame>
+    <MarkdownDocsContents markdown={content} markdownLocation={markdownLocationProp}>
+      {({ contents, markdownLocation }) => (
+        <AppFrame>
+          <AppTableOfContents content={content} />
+          <AppContent>
+            <div className={classes.header}>
+              <Breadcrumbs />
+              <div className={classes.headerSpace} />
+              <EditPage
+                markdownLocation={markdownLocation}
+                sourceCodeRootUrl={SOURCE_CODE_ROOT_URL}
+              />
+            </div>
+            <MdElement content={content} />
+          </AppContent>
+        </AppFrame>
+      )}
+    </MarkdownDocsContents>
   );
 };
 
