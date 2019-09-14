bf5d4f102fc4efbdc535865bbb20d1a652497c7b
Gritsch Markus <markus.gritsch@millipede.me>
2019-09-14 15:41:56 +0200

    Remove artifiact. In previous next versions the type LinkProps was not exported.

--- docs/src/modules/components/common/button/NextLinkMuiLink.tsx
+++ docs/src/modules/components/common/button/NextLinkMuiLink.tsx
@@ -1,17 +1,15 @@
 import { Omit } from '@material-ui/core';
 import MuiLink, { LinkProps as MuiLinkProps } from '@material-ui/core/Link';
+import { LinkProps as NextLinkProps } from 'next/link';
 import React from 'react';
 
-import { LinkProps as NextLinkProps } from '../../../../../../src/typings/link';
-
-// import { LinkProps as NextLinkProps } from 'next/link';
 type ComponentWorkaroundProps<P extends {}> = P & {
   component?: React.ComponentType<P>;
 };
 
 const NextLinkMuiLink = React.forwardRef<
   HTMLButtonElement,
-  Omit<MuiLinkProps, "component"> & ComponentWorkaroundProps<NextLinkProps>
+  Omit<MuiLinkProps, 'component'> & ComponentWorkaroundProps<NextLinkProps>
 >(({ children, ...props }, ref) => {
   return (
     <MuiLink {...props} ref={ref}>
