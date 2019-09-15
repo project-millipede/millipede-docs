982157a505b72e9cbbc3769246f9d5220fb5d9d9
Gritsch Markus <markus.gritsch@millipede.me>
2019-09-16 00:30:09 +0200

    Missed React import
    Upgrade next to latest canary

--- pages/common/dataflow/comparison/index.tsx
+++ pages/common/dataflow/comparison/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 
 import MDXContentLoader, { MDXContentLoaderProps } from '../../../../docs/src/modules/components/loader/MDXContentLoader';
 
