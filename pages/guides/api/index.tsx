ec2149fd964c558335a154675b0bf5a2d1617592
Gritsch Markus <markus.gritsch@millipede.me>
2019-09-16 00:30:09 +0200

    Missed React import
    Upgrade next to latest canary

--- pages/guides/api/index.tsx
+++ pages/guides/api/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 
 import MDContentLoader, { MDContentLoaderProps } from '../../../docs/src/modules/components/loader/MDContentLoader';
 
