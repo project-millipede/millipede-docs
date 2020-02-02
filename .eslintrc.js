4eb47f751d7974e0801083456c4083e24d3f99d6
gurkerl83 <markus_gritsch@gmx.de>
2020-02-02 12:53:19 +0100

    Fix ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,80 +1,74 @@
-const path = require('path');
-
 module.exports = {
   env: {
     browser: true
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
+  /*
   parserOptions: {
-    project: [path.resolve(__dirname, './tsconfig.json')]
+    project: [
+      path.resolve(__dirname, "./tsconfig.json"),
+      path.resolve(__dirname, "./webpack/tsconfig.json")
+    ]
   },
-  plugins: ['@typescript-eslint'],
+  */
+  plugins: ["@typescript-eslint"],
   extends: [
-    'airbnb-base',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:mdx/recommended',
-    'prettier',
-    'prettier/@typescript-eslint'
+    "airbnb-base",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint"
   ],
   settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.js', '.ts', '.tsx']
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
     },
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
       }
     }
   },
   rules: {
-    'import/extensions': [
-      'error',
-      'ignorePackages',
+    "import/extensions": [
+      "error",
+      "ignorePackages",
       {
-        js: 'never',
-        jsx: 'never',
-        mjs: 'never',
-        ts: 'never',
-        tsx: 'never'
+        js: "never",
+        jsx: "never",
+        mjs: "never",
+        ts: "never",
+        tsx: "never"
       }
     ],
-    'import/prefer-default-export': 'off',
-    'import/no-unresolved': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
+    "import/prefer-default-export": "off",
+    "import/no-unresolved": "off",
+    "import/no-extraneous-dependencies": [
+      "error",
       {
-        devDependencies: ['webpack/*']
+        devDependencies: ["webpack/*"]
       }
     ],
-    'no-use-before-define': 'off',
-    'lines-between-class-members': [
-      'error',
-      'always',
+    "no-use-before-define": "off",
+    "lines-between-class-members": [
+      "error",
+      "always",
       { exceptAfterSingleLine: true }
     ],
-    'no-return-assign': 'error',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
-    'no-shadow': 'off',
-    'no-param-reassign': [
-      'error',
+    "no-return-assign": "error",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/array-type": ["error", { default: "generic" }],
+    "no-shadow": "off",
+    "no-param-reassign": [
+      "error",
       {
         props: true,
-        ignorePropertyModificationsFor: ['draftState']
+        ignorePropertyModificationsFor: ["draftState"]
       }
     ]
-  },
-  overrides: [
-    {
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off'
-      }
-    }
-  ]
+  }
 };
