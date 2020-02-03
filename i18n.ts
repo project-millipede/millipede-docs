7d0591c89886922cb1128c2ee8337ff23b293360
gurkerl83 <markus_gritsch@gmx.de>
2020-02-03 09:22:59 +0100

    Check runtime target

--- i18n.ts
+++ i18n.ts
@@ -1,17 +1,32 @@
 import fs from 'fs';
-import path from 'path';
 import NextI18Next from 'next-i18next-serverless';
 import getConfig from 'next/config';
+import path from 'path';
+
+const isBrowser = typeof window !== 'undefined';
 
 const { publicRuntimeConfig } = getConfig();
 
-const locales = fs.existsSync(
-  path.join(publicRuntimeConfig.PROJECT_ROOT, './locales/en/common.json')
-);
-const publicLocales = fs.readFileSync(
-  path.join(publicRuntimeConfig.PROJECT_ROOT, './public/locales/en/common.json')
-);
+const { PROJECT_ROOT } = publicRuntimeConfig;
 
+console.log('PROJECT_ROOT: ', PROJECT_ROOT);
+
+const locales = !isBrowser
+  ? fs.existsSync(
+      path.join(publicRuntimeConfig.PROJECT_ROOT, './locales/en/common.json')
+    )
+  : false;
+
+const publicLocales = !isBrowser
+  ? fs.readFileSync(
+      path.join(
+        publicRuntimeConfig.PROJECT_ROOT,
+        './public/locales/en/common.json'
+      )
+    )
+  : false;
+
+console.log('isBrowser: ', isBrowser);
 console.log('locales exists: ', locales);
 console.log('publicLocales exists: ', publicLocales);
 
