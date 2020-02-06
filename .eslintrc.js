720d97d5c453643bb401a36d7934eaa2d10465aa
gurkerl83 <markus_gritsch@gmx.de>
2020-02-06 09:49:46 +0100

    Fix long outstanding es-lint issue with eslint-plugin-import. Restructure eslint setup from .json to .js. Properly setup parserOptions / project. Add webpack in typescript include of tsconfig.
    Update eslint dependencies
    
    Remove unused code

---
+++ .eslintrc.js
@@ -0,0 +1,79 @@
+const path = require('path');
+
+module.exports = {
+  env: {
+    browser: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: [path.resolve(__dirname, './tsconfig.json')]
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+      }
+    }
+  },
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        mjs: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['webpack/*']
+      }
+    ],
+    'no-use-before-define': 'off',
+    'lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true }
+    ],
+    'no-return-assign': 'error',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+    'no-shadow': 'off',
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: ['draftState']
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    }
+  ]
+};
