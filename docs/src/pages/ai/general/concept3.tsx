4803037f89bf73b863408faff35aeef0e3847282
Gritsch Markus <markus.gritsch@millipede.me>
2020-04-19 21:37:37 +0200

    Remove unused code for ai / general / third diagram.

--- docs/src/pages/ai/general/concept3.tsx
+++ docs/src/pages/ai/general/concept3.tsx
@@ -12,34 +12,21 @@ const useStyles = makeStyles(() =>
       display: 'flex',
       justifyContent: 'center'
     },
-    rowLeft: {
-      margin: '50px 0',
-      display: 'flex'
-      // justifyContent: 'center'
-    },
-    rowRight: {
-      margin: '50px 0',
-      display: 'flex',
-      // justifyContent: 'center'
-      flexDirection: 'row-reverse'
-    },
     row: {
       margin: '50px 0',
       display: 'flex',
       justifyContent: 'space-between'
+    },
+    boxWrapper: {
+      padding: '10px',
+      border: '3px solid black',
+      margin: '50px 0'
     },
     box: {
       padding: '10px',
       border: '3px solid black',
       maxWidth: '150px'
     },
-
-    boxWrapper: {
-      padding: '10px',
-      border: '3px solid black',
-      margin: '50px 0'
-    },
-
     title: {
       textAlign: 'center',
       fontWeight: 'bold'
@@ -101,26 +88,35 @@ const Diagram = () => {
       </div>
 
       <div className={classes.boxWrapper}>
-        {/* <div className={classes.rowRight}> */}
-        <div className={classes.rowMiddle}>
+        <Typography variant='subtitle1' className={classes.title}>
+          {t('dedicated_render_process')}
+        </Typography>
+
+        <div className={classes.row}>
+          <ArcherElement
+            id='react_observe'
+            relations={[
+              {
+                targetId: 'coordination',
+                targetAnchor: 'left',
+                sourceAnchor: 'bottom'
+              }
+            ]}
+          >
+            <div className={classes.box}>
+              <Typography variant='subtitle1' className={classes.title}>
+                {`${t('react_observe')}`}
+              </Typography>
+            </div>
+          </ArcherElement>
+
           <ArcherElement
             id='render'
             relations={[
               {
-                targetId: 'coordination',
-                // targetAnchor: 'right',
-                targetAnchor: 'top',
-                sourceAnchor: 'bottom',
-
-                // targetId: 'react_observe',
-                // targetAnchor: 'top',
-                // sourceAnchor: 'bottom',
-
-                label: (
-                  <div
-                  // style={{ marginTop: '-20px' }}
-                  >{`${t('react_observe')}`}</div>
-                )
+                targetId: 'react_observe',
+                targetAnchor: 'right',
+                sourceAnchor: 'left'
               }
             ]}
           >
@@ -132,30 +128,7 @@ const Diagram = () => {
           </ArcherElement>
         </div>
 
-        {/* <div className={classes.boxWrapper}> */}
-        {/* <Typography variant='subtitle1' className={classes.title}>
-          {t('target_application')}
-        </Typography> */}
-
         <div className={classes.rowMiddle}>
-          {/* <div className={classes.row}> */}
-          {/* <ArcherElement
-            id='observe_react'
-            relations={[
-              {
-                targetId: 'coordination',
-                targetAnchor: 'left',
-                sourceAnchor: 'right'
-              }
-            ]}
-          >
-            <div className={classes.box}>
-              <Typography variant='subtitle1' className={classes.title}>
-                {`${t('observe_react')}`}
-              </Typography>
-            </div>
-          </ArcherElement> */}
-
           <ArcherElement id='coordination'>
             <Box className={classes.box}>
               <Typography variant='subtitle1' className={classes.title}>
@@ -163,103 +136,16 @@ const Diagram = () => {
               </Typography>
             </Box>
           </ArcherElement>
-
-          {/* <ArcherElement
-            id='react_observe'
-            relations={[
-              {
-                targetId: 'coordination',
-                targetAnchor: 'right',
-                sourceAnchor: 'left'
-              }
-            ]}
-          >
-            <div className={classes.box}>
-              <Typography variant='subtitle1' className={classes.title}>
-                {`${t('react_observe')}`}
-              </Typography>
-            </div>
-          </ArcherElement> */}
         </div>
-        {/* </div> */}
 
-        {/* <div className={classes.row}>
-        <ArcherElement
-          id='communication'
-          relations={[
-            {
-              targetId: 'preparation',
-              targetAnchor: 'left',
-              sourceAnchor: 'right'
-            }
-          ]}
-        >
-          <div className={classes.box}>
-            <Typography variant='subtitle1' className={classes.title}>
-              {`${t('communication_process')} ${t(
-                'communication_process_abbreviation'
-              )}`}
-            </Typography>
-          </div>
-        </ArcherElement>
-        <ArcherElement
-          id='preparation'
-          relations={[
-            {
-              targetId: 'shared_render',
-              targetAnchor: 'right',
-              sourceAnchor: 'bottom'
-            }
-          ]}
-        >
-          <Box className={classes.box}>
-            <Typography variant='subtitle1' className={classes.title}>
-              {`${t('preparation_process')} ${t(
-                'preparation_process_abbreviation'
-              )}`}
-            </Typography>
-          </Box>
-        </ArcherElement>
-        <ArcherElement
-          id='preparation'
-          relations={[
-            {
-              targetId: 'shared_render',
-              targetAnchor: 'right',
-              sourceAnchor: 'bottom'
-            }
-          ]}
-        >
-          <Box className={classes.box}>
-            <Typography variant='subtitle1' className={classes.title}>
-              {`${t('preparation_process')} ${t(
-                'preparation_process_abbreviation'
-              )}`}
-            </Typography>
-          </Box>
-        </ArcherElement>
-      </div> */}
-
-        {/* <div className={classes.rowLeft}> */}
-        <div className={classes.rowMiddle}>
+        <div className={classes.row}>
           <ArcherElement
             id='custom_render'
             relations={[
               {
-                targetId: 'coordination',
-                // targetAnchor: 'left',
-                targetAnchor: 'bottom',
-                sourceAnchor: 'top',
-
-                // targetId: 'observe_react',
-                // targetAnchor: 'bottom',
-                // sourceAnchor: 'top',
-
-                label: (
-                  <div
-                  // style={{ marginTop: '-20px' }}
-                  >{`${t('observe_react')}`}</div>
-                )
+                targetId: 'observe_react',
+                targetAnchor: 'left',
+                sourceAnchor: 'right'
               }
             ]}
           >
@@ -268,6 +154,23 @@ const Diagram = () => {
                 {`${t('custom_render_process')} ${t(
                   'custom_render_process_abbreviation'
                 )}`}
+              </Typography>
+            </div>
+          </ArcherElement>
+
+          <ArcherElement
+            id='observe_react'
+            relations={[
+              {
+                targetId: 'coordination',
+                targetAnchor: 'right',
+                sourceAnchor: 'top'
+              }
+            ]}
+          >
+            <div className={classes.box}>
+              <Typography variant='subtitle1' className={classes.title}>
+                {`${t('observe_react')}`}
               </Typography>
             </div>
           </ArcherElement>
