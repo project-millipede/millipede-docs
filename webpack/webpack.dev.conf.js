23c3ca1b23f3740cf6e4dcf337808d7e8fdcc716
gurkerl83 <markus_gritsch@gmx.de>
2020-11-10 23:24:05 +0100

    Migrate webpack to V5
    From now on, this project uses Yarn in favor of NPM. Increase the dependency version of webpack. Add resolutions section in package.json. The section resolutions get used by Yarn and get ignored by NPM. NPM does not implement selective dependency resolutions for which the field resolutions in package.json is all about.
    
    Next installations also bring its webpack installation in its node-modules folder, V4. Building an application identifies both webpack versions, V5 from an application, and V4 from next. This situation is problematic, similar to when a library serves its own react version in its node-module folder on installation.
    
    Change webpack configurations, according to the webpack V5 documentation. Previous node.something: 'empty' has to be replaced with resolve.fallback.something: false.
    
    Remove concurrent loading strategy through happypack in favor of fast caches of webpack 5.

--- webpack/webpack.dev.conf.js
+++ webpack/webpack.dev.conf.js
@@ -4,141 +4,50 @@ const HappyPack = require('happypack');
 
 const pkg = require('../package.json');
 
-const setFs = isServer => {
-  // Fixes npm packages that depend on `fs` module
+const getFallback = isServer => {
   if (!isServer) {
-    return 'empty';
+    return {
+      fs: false
+    };
   }
-  return true;
 };
 
 const webpackConfig = ({ isServer }) => {
+  
   return {
     mode: 'development',
 
-    node: {
-      fs: setFs(isServer)
-    },
-
     resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      fallback: {
+        ...getFallback(isServer)
+      }
     },
 
     module: {
       rules: [
         {
           test: /\.(ts|tsx)$/,
-          loader: 'happypack/loader?id=ts'
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: true
+              },
+            },
+          ],
         },
         {
           test: /\.mdx$/,
-          loader: 'happypack/loader?id=mdx'
-        },
-        {
-          test: /\.md$/,
-          use: 'raw-loader'
-        }
-      ]
-    },
-
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env': {
-          PROJECT_VERSION: JSON.stringify(pkg.version)
-        }
-      }),
-      new HappyPack({
-        // id declaration references to the loader definition: 'happypack/loader?id=ts'
-        id: 'ts',
-        threads: 3,
-        loaders: [
-          {
-            path: 'ts-loader',
-            query: { happyPackMode: true },
-            options: {
-              // disable type checker - typechecking is handeled by fork-ts-checker-webpack-plugin
-              transpileOnly: true
-            }
-          }
-        ]
-      }),
-      new HappyPack({
-        id: 'mdx',
-        threads: 2,
-        loaders: [
-          {
-            path: 'babel-loader'
-          },
-          {
-            loader: path.join(__dirname, '../dist/loader/mdx-custom-loader')
-          }
-        ]
-      })
-    ]
-  };
-};
-
-module.exports = webpackConfig;
-
-/* webpack.prod.conf in ts */
-
-/*
-import path from 'path';
-import webpack from 'webpack';
-
-import pkg from '../package.json';
-*/
-
-// import * as options from './remarkReactComponents';
-
-/*
-const setFs = isServer => {
-  // Fixes npm packages that depend on `fs` module
-  if (!isServer) {
-    return 'empty';
-  }
-  return true;
-};
-
-// const webpackConfig = ({ isServer }): webpack.Configuration => {
-const webpackConfig = ({ isServer }) => {
-  return {
-    mode: 'production',
-
-    node: {
-      fs: setFs(isServer)
-    },
-
-    // The context is two levels out, because next does currently not support
-    // configurations (next.config) in typescript
-    // context: path.resolve(__dirname, '../../'),
-    // context: path.resolve(__dirname, '../'),
-
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
-    },
-
-    module: {
-      rules: [
-        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-        { test: /\.tsx?$/, loader: 'ts-loader' },
-        {
-          test: /\.mdx$/,
-          // use: [path.join(__dirname, './loader/mdx-custom-loader')]
           use: [
             {
               loader: 'babel-loader'
             },
             {
-              // loader: path.join(__dirname, './loader/mdx-custom-loader'),
-              loader: path.join(__dirname, '../dist/loader/mdx-custom-loader'),
-              options: {
-                remarkPlugins: [
-                  // options
-                ]
-              }
-            }
-          ]
+              loader: path.join(__dirname, '../dist/loader/mdx-custom-loader')
+            },
+          
+          ],
         },
         {
           test: /\.md$/,
@@ -152,13 +61,10 @@ const webpackConfig = ({ isServer }) => {
         'process.env': {
           PROJECT_VERSION: JSON.stringify(pkg.version)
         }
-      })
+      }),
     ]
+
   };
 };
 
-// const webpackConfig: webpack.Configuration = merge(baseWebpackConfig, webpackDevConfig);
-// export default webpackConfig;
-
-// export default webpackConfig;
-*/
+module.exports = webpackConfig;
