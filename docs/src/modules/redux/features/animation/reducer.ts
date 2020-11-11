6065b520a6d2787ba051c1383a19c3414c20af75
Gritsch Markus <markus.gritsch@millipede.me>
2020-11-11 17:02:05 +0100

    Remove unused code and dependencies

--- docs/src/modules/redux/features/animation/reducer.ts
+++ docs/src/modules/redux/features/animation/reducer.ts
@@ -1,32 +1,20 @@
-import { Area, Device, Progress, ProgressViewport, Szenario } from '../../../../../../src/typings/animation';
+import { Area, Device, Szenario } from '../../../../../../src/typings/animation';
 import { StoreAction } from '../actionType';
-import {
-  CHANGE_AREA,
-  CHANGE_DEVICE,
-  CHANGE_SZENARIO,
-  SET_PROGRESS_OFFSET_Y,
-  SET_PROGRESS_VIEWPORT,
-  UPDATE_PROGRESS,
-} from './actionTypes';
+import { CHANGE_AREA, CHANGE_DEVICE, CHANGE_SZENARIO } from './actionTypes';
 
 interface Props {
   device: Device;
   szenario: Szenario;
   area: Area;
-  progress: Progress;
-  progressOffsetY: number;
-  progressViewport: ProgressViewport;
 }
 
 export const initialState: Props = {
   device: Device.Desktop,
   szenario: Szenario.Default,
-  area: Area.Local,
-  progress: {},
-  progressOffsetY: 0,
-  progressViewport: {}
+  area: Area.Local
 };
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const animationReducer = (state = initialState, action: StoreAction) => {
   switch (action.type) {
     case CHANGE_DEVICE:
@@ -43,29 +31,6 @@ const animationReducer = (state = initialState, action: StoreAction) => {
       return {
         ...state,
         area: action.payload.area
-      };
-    case UPDATE_PROGRESS:
-      return {
-        ...state,
-        progress: action.payload.progress
-      };
-    case SET_PROGRESS_OFFSET_Y:
-      return {
-        ...state,
-        progressOffsetY: action.payload.progressOffsetY
-      };
-    // case SET_PROGRESS_VIEWPORT:
-    //   return {
-    //     ...state,
-    //     progressViewport: action.payload.progressViewport
-    //   };
-    case SET_PROGRESS_VIEWPORT:
-      return {
-        ...state,
-        progressViewport: {
-          ...state.progressViewport,
-          [action.payload.index]: action.payload.progressViewport
-        }
       };
     default:
       return state;
