27a1954da60cf06c68b9efacd7ac2d028de37276
Gritsch Markus <markus.gritsch@millipede.me>
2020-11-11 13:28:37 +0100

    Refactor components interactive head and toc
    Change data structure for persisting elements in viewport from Set to Object.

---
+++ docs/src/modules/recoil/features/scroll/page/reducer.ts
@@ -0,0 +1,42 @@
+import { atom } from 'recoil';
+
+export interface State {
+  scrollItems: { [key: string]: string };
+}
+
+export const initialState: State = {
+  scrollItems: {}
+};
+
+export const scrollItemsState = atom({
+  key: 'scrollItems',
+  default: initialState
+});
+
+const addItem = (state: State, newItem: string): State => {
+  return {
+    ...state,
+    scrollItems: {
+      ...state.scrollItems,
+      [newItem]: newItem
+    }
+  };
+};
+
+const removeItem = (state: State, itemToRemove: string): State => {
+  const {
+    [itemToRemove]: deletedItem,
+    ...objectWithoutDeletedProp
+  } = state.scrollItems;
+  return {
+    ...state,
+    scrollItems: {
+      ...objectWithoutDeletedProp
+    }
+  };
+};
+
+export const scrollItemsReducer = {
+  addItem,
+  removeItem
+};
