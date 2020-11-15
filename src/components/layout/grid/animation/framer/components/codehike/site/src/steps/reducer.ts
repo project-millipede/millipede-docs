8b18cd0f1a5169f5fb9be0b298a4bd6d4237b424
Gritsch Markus <markus.gritsch@millipede.me>
2020-11-15 14:01:45 +0100

    Control of structure and composition of seq diags
    Auto Play component
    The component enables the individual steps contained in a sequence diagram to be run through as a series. An individual animation time is assigned to each single step; once this time has elapsed, the animation jumps to the next single step.
    
    Progressive Step Builder
    Allows the interactive expansion of a sequence diagram by appending single steps to an existing sequence diagram at the end.

---
+++ src/components/layout/grid/animation/framer/components/codehike/site/src/steps/reducer.ts
@@ -0,0 +1,107 @@
+export const init = (maxStepsCount: number) => {
+  return <const>{ type: 'INIT', maxStepsCount };
+};
+
+export const reset = () => {
+  return <const>{ type: 'RESET' };
+};
+
+export const toggle = () => {
+  return <const>{ type: 'TOGGLE' };
+};
+export const prev = () => {
+  return <const>{ type: 'PREV' };
+};
+
+export const next = () => {
+  return <const>{ type: 'NEXT' };
+};
+
+export const seek = (target: number) => {
+  return <const>{ type: 'SEEK', target };
+};
+
+export const auto = () => {
+  return <const>{ type: 'AUTO' };
+};
+
+export type Action = ReturnType<
+  | typeof init
+  | typeof reset
+  | typeof toggle
+  | typeof prev
+  | typeof next
+  | typeof seek
+  | typeof auto
+>;
+
+export interface StepState {
+  target: number;
+  playing: boolean;
+  back: boolean;
+  maxStepsCount?: number;
+}
+
+export const initialState: StepState = {
+  target: 0,
+  playing: false,
+  back: false,
+  maxStepsCount: 0
+};
+
+export const reducer = (state: StepState, action: Action) => {
+  const { target, playing, maxStepsCount } = state;
+  switch (action.type) {
+    case 'INIT': {
+      return {
+        ...state,
+        maxStepsCount: action.maxStepsCount
+      };
+    }
+    case 'RESET':
+      return {
+        ...state,
+        ...initialState
+      };
+    case 'TOGGLE': {
+      return {
+        ...state,
+        playing: !playing
+      };
+    }
+    case 'PREV': {
+      const newTarget = Math.max(target - 1, 0);
+      return {
+        ...state,
+        target: newTarget,
+        back: newTarget < target
+      };
+    }
+    case 'NEXT': {
+      const newTarget = Math.min(target + 1, maxStepsCount);
+      return {
+        ...state,
+        target: newTarget,
+        back: newTarget < target
+      };
+    }
+    case 'SEEK': {
+      const newTarget = action.target;
+      return {
+        ...state,
+        target: newTarget,
+        back: newTarget < target
+      };
+    }
+    case 'AUTO': {
+      const newTarget = Math.min(target + 1, maxStepsCount);
+      return {
+        ...state,
+        target: newTarget,
+        back: newTarget < target
+      };
+    }
+    default:
+      return state;
+  }
+};
