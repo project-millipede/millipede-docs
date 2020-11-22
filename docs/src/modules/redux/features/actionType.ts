d7fce007afc3d4f617a356de5fc668c9e7635a63
Gritsch Markus <markus.gritsch@millipede.me>
2020-11-22 13:29:22 +0100

    Remove feature "scroll"
    The feature "scroll" got migrated to recoil a while ago.

--- docs/src/modules/redux/features/actionType.ts
+++ docs/src/modules/redux/features/actionType.ts
@@ -2,21 +2,18 @@ import { ActionType } from 'typesafe-actions';
 
 import * as Animation from './animation/actions';
 import * as Navigation from './navigation/actions';
-import * as Scroll from './scroll/actions';
 import * as Theme from './theme/actions';
 import * as Timeline from './timeline/actions';
 import * as View from './view/actions';
 
 export type AnimationActions = ActionType<typeof Animation>;
 export type NavigationActions = ActionType<typeof Navigation>;
-export type ScrollActions = ActionType<typeof Scroll>;
 export type ThemeActions = ActionType<typeof Theme>;
 export type TimelineActions = ActionType<typeof Timeline>;
 export type ViewActions = ActionType<typeof View>;
 
 export type StoreAction =
   | AnimationActions
-  | ScrollActions
   | NavigationActions
   | ThemeActions
   | TimelineActions
