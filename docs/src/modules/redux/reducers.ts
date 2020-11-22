3ea8f4ddeda188b8afd76471d16ca46716981b27
Gritsch Markus <markus.gritsch@millipede.me>
2020-11-22 13:29:22 +0100

    Remove feature "scroll"
    The feature "scroll" got migrated to recoil a while ago.

--- docs/src/modules/redux/reducers.ts
+++ docs/src/modules/redux/reducers.ts
@@ -2,7 +2,6 @@ import { StateType } from 'typesafe-actions';
 
 import animation from './features/animation/reducer';
 import navigation from './features/navigation/reducer';
-import scroll from './features/scroll/reducer';
 import theme from './features/theme/reducer';
 import timeline from './features/timeline/reducer';
 import view from './features/view/reducer';
@@ -10,7 +9,6 @@ import view from './features/view/reducer';
 const reducers = {
   animation,
   navigation,
-  scroll,
   theme,
   timeline,
   view
@@ -20,7 +18,6 @@ export type RootState = StateType<typeof reducers>;
 
 export type AnimationState = StateType<typeof animation>;
 export type NavigationState = StateType<typeof navigation>;
-export type ScrollState = StateType<typeof scroll>;
 export type ThemeState = StateType<typeof theme>;
 export type TimelineState = StateType<typeof timeline>;
 export type ViewState = StateType<typeof view>;
@@ -28,7 +25,6 @@ export type ViewState = StateType<typeof view>;
 export type StoreState =
   | AnimationState
   | NavigationState
-  | ScrollState
   | ThemeState
   | TimelineState
   | ViewState;
