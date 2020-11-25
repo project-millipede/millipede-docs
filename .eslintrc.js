b510bf225952112a8dfb61d68ef2bc6def5a5469
gurkerl83 <markus_gritsch@gmx.de>
2020-11-25 15:16:15 +0100

    De-structure es-lint into smaller portions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,72 +1,18 @@
-const path = require('path');
-
 module.exports = {
   env: {
     browser: true,
     jest: true
   },
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: [path.resolve(__dirname, './tsconfig.json')]
-  },
   plugins: ['@typescript-eslint'],
-  extends: [    
-    // 'airbnb-base',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:mdx/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-    // 'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript'
+  extends: [
+    'eslint:recommended',
+    './config/eslint/typescript.yml',
+    './config/eslint/import.yml',
+    './config/eslint/prettier.yml',
+    './config/eslint/mdx.yml'
   ],
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.js', '.ts', '.tsx']
-    },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
-      }
-    }
-  },
   rules: {
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        mjs: 'never',
-        ts: 'never',
-        tsx: 'never'
-      }
-    ],
-    'import/prefer-default-export': 'off',
-    'import/no-unresolved': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: ['webpack/*']
-      }
-    ],
-    'no-use-before-define': 'off',
-    'lines-between-class-members': [
-      'error',
-      'always',
-      { exceptAfterSingleLine: true }
-    ],
-    'no-return-assign': 'error',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
-    'no-shadow': 'off',
     'no-param-reassign': [
       'error',
       {
@@ -74,13 +20,5 @@ module.exports = {
         ignorePropertyModificationsFor: ['draftState']
       }
     ]
-  },
-  overrides: [
-    {
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off'
-      }
-    }
-  ]
+  }
 };
