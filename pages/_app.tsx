f6a4b069c20724ab7e0c8640ee9fd450820ceb82
gurkerl83 <markus_gritsch@gmx.de>
2020-11-29 14:25:05 +0100

    Integration of workspaces into existing components
    Remove jss server side setup; jss is not used anymore.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,73 +1,52 @@
 import '../docs/css/diagram.css';
 
+import { defaultAnalytics } from '@app/analytics';
+import { Portal } from '@app/components';
 import { HouxProvider } from '@app/houx';
-import { enableMapSet } from 'immer';
+import { AppFrame, AppWrapper, reducers as layoutReducers } from '@app/layout';
 import { NextComponentType } from 'next';
 import I18nProvider from 'next-translate/I18nProvider';
 import { AppContext, AppInitialProps, AppProps } from 'next/app';
 import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
-import ReactGA from 'react-ga';
+import React from 'react';
 import { RecoilRoot } from 'recoil';
+import { AnalyticsProvider } from 'use-analytics';
 
-import { AppFrame } from '../docs/src/modules/components/AppFrame';
-import { AppWrapper } from '../docs/src/modules/components/AppWrapper';
 import { loadFAIcons } from '../docs/src/modules/components/icon/FAIconLoader';
-import reducers from '../docs/src/modules/redux/reducers';
+import { reducers } from '../docs/src/modules/redux/reducers';
 import { detectDevice } from '../docs/src/modules/utils/device';
-import { PortalProvider } from '../src/components/layout/grid/animation/framer/components/shared/portals/portals';
-
-// import { appWithTranslation } from '../i18n';
-
-// const TRACKING_CODE_MILLIPEDE = 'UA-151314446-1';
-const TRACKING_CODE_PRIVACY_SHIELD = 'UA-154899959-1';
 
 loadFAIcons();
-
-enableMapSet();
-
-// interface PageProps {
-//   isMobile: boolean;
-// }
-
-// interface Props extends AppContext {
-//   pageProps: PageProps;
-// }
 
 const MillipedeApp: NextComponentType<
   AppContext,
   AppInitialProps,
   AppProps
 > = ({ Component, pageProps }) => {
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentNode.removeChild(jssStyles);
-    }
-    // ReactGA.initialize(TRACKING_CODE_MILLIPEDE);
-    ReactGA.initialize(TRACKING_CODE_PRIVACY_SHIELD);
-  }, []);
-
   const { isMobile } = pageProps;
 
   const { locale } = useRouter();
 
   return (
     // eslint-disable-next-line no-underscore-dangle
-    <I18nProvider lang={locale} namespaces={pageProps._ns}>
-      <PortalProvider>
-        <HouxProvider reducers={reducers} logDispatchedActions>
-          <RecoilRoot>
-            <AppWrapper isMobile={isMobile}>
-              <AppFrame>
-                <Component {...pageProps} />
-              </AppFrame>
-            </AppWrapper>
-          </RecoilRoot>
-        </HouxProvider>
-      </PortalProvider>
-    </I18nProvider>
+    <HouxProvider
+      reducers={{ ...reducers, ...layoutReducers }}
+      logDispatchedActions
+    >
+      <I18nProvider lang={locale} namespaces={pageProps._ns}>
+        <AnalyticsProvider instance={defaultAnalytics}>
+          <Portal.Portal.PortalProvider>
+            <RecoilRoot>
+              <AppWrapper isMobile={isMobile}>
+                <AppFrame>
+                  <Component {...pageProps} />
+                </AppFrame>
+              </AppWrapper>
+            </RecoilRoot>
+          </Portal.Portal.PortalProvider>
+        </AnalyticsProvider>
+      </I18nProvider>
+    </HouxProvider>
   );
 };
 
