cec31dc71e3604eedfe271cb82a77cc0580d2e90
gurkerl83 <markus_gritsch@gmx.de>
2020-11-29 14:36:04 +0100

    Cleanup mdx parser

--- packages/mdx-loader/src/mdx-parser.tsx
+++ packages/mdx-loader/src/mdx-parser.tsx
@@ -2,31 +2,23 @@ import mdx from '@mdx-js/mdx';
 import matter from 'gray-matter';
 import remarkSlug from 'remark-slug';
 
-// import { readingTime } from 'reading-time-estimator';
 export interface ParserOptions {
-  filePath?: string; // the path to the file
-  babel?: boolean; // whether to transpile otherwise es6 / jsx output returned from mdx
+  filePath?: string;
   remarkPlugins?: Array<() => (tree: any) => any>;
   rehypePlugins?: Array<(options: any) => (tree: any) => void>;
-  injectCode?: Array<string>;
-}
-
-interface ParserProps {
-  source: string; // raw mdx content
-  options: ParserOptions;
 }
 
 export const compile = async (
   src: string,
   options: ParserOptions,
-  injectRemarkPlugins: Array<() => (tree: any) => any>,
-  injectRehypePlugins: Array<(options: any) => (tree: any) => void>
+  injectRemarkPlugins: Array<() => (tree: any) => any> = [],
+  injectRehypePlugins: Array<(options: any) => (tree: any) => void> = []
 ) => {
   const { remarkPlugins, rehypePlugins } = options;
 
   const jsx = await mdx(src, {
-    remarkPlugins: [].concat(injectRemarkPlugins).concat(remarkPlugins || []),
-    rehypePlugins: [].concat(injectRehypePlugins).concat(rehypePlugins || [])
+    remarkPlugins: injectRemarkPlugins.concat(remarkPlugins),
+    rehypePlugins: injectRehypePlugins.concat(rehypePlugins)
   });
   return jsx;
 };
@@ -41,18 +33,14 @@ export async function parser(raw: string, options: ParserOptions) {
     content,
     options,
     injectRemarkPlugins,
-    injectRemarkPlugins
+    injectRehypePlugins
   );
-
-  // const timeToRead = readingTime(raw);
 
   const code = [
     `import React from 'react'`,
     `import { mdx } from '@mdx-js/react'`,
     `export const meta = ${JSON.stringify(meta)}`,
     `export const raw = ${JSON.stringify(content)}`,
-    // `export const timeToRead = ${JSON.stringify(timeToRead)}`,
-    `export const timeToRead = ''`,
     result
   ].join('\n');
 
