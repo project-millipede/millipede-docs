9aa3b1150394df69911555ba81daadf9b0da140f
gurkerl83 <markus_gritsch@gmx.de>
2020-12-09 15:49:13 +0100

    Onboard workspace utils
    No integration provided, for this reason the build gets broken

--- docs/src/modules/utils/index.ts
+++
@@ -1,5 +0,0 @@
-import * as CollectionUtil from './collection';
-import * as MathUtil from './math';
-import * as StringUtil from './string';
-
-export { CollectionUtil, StringUtil, MathUtil };
---
+++ packages/utils/src/index.ts
@@ -0,0 +1,7 @@
+import * as CollectionUtil from './collection';
+import * as DeviceUtil from './device';
+import * as LogUtil from './log';
+import * as MathUtil from './math';
+import * as StringUtil from './string';
+
+export { CollectionUtil, MathUtil, LogUtil, DeviceUtil, StringUtil };
