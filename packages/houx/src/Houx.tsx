8c08ef9411e58acb66165a2a8295ef1c59ecf19e
gurkerl83 <markus_gritsch@gmx.de>
2020-12-09 20:03:04 +0100

    Rewrite the houx module
    From now on, it is possible to add and remove reducer on the fly.

--- packages/houx/src/Houx.tsx
+++ packages/houx/src/Houx.tsx
@@ -1,65 +1,79 @@
-import React, { createContext, Dispatch, FC, useCallback, useContext, useReducer } from 'react';
+import { CollectionUtil } from '@app/utils';
+import { createContext, Dispatch, FC, useContext, useReducer } from 'react';
 import { Action, StateType } from 'typesafe-actions';
 
 interface ContextType {
   state: StateType<any>;
   dispatch: Dispatch<Action>;
+  addReducer: (reducer: any) => void;
+  removeReducer: (name: string) => void;
 }
 
-const Context = createContext<ContextType>({
-  state: null,
-  dispatch: null
-});
-const composeReducers = (reducers: ReducerMap) => (state, action) => {
-  const combinedReducers = Object.entries(reducers).reduce(
-    (acc, [namespace, reducer]) => {
-      return { ...acc, [namespace]: reducer(state[namespace], action) };
-    },
-    {}
-  );
-  return combinedReducers;
-};
-
 type ReducerType = (state: any, action: any) => any;
+
 interface ReducerMap {
   [key: string]: ReducerType;
 }
-interface HouxProviderProps {
-  logDispatchedActions?: boolean;
-  reducers: ReducerMap;
+
+const StoreContext = createContext<ContextType>({
+  state: {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  dispatch: () => {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  addReducer: () => {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  removeReducer: () => {}
+});
+
+const [addReducer, combinedReducer, removeReducer] = (() => {
+  let reducers: ReducerMap = {};
+
+  const addReducer = reducer => {
+    reducers = { ...reducers, ...reducer };
+  };
+
+  const removeReducer = (name: string) => {
+    reducers = CollectionUtil.Object.removePropertyFromObject(reducers, name);
+  };
+
+  const combinedReducer = (initialState = {}, action = {}) => {
+    return Object.entries(reducers).reduce((state, [name, reducer]) => {
+      const resultingState = {
+        ...state,
+        [name]: reducer(state[name], action) as any
+      };
+      return resultingState;
+    }, initialState);
+  };
+  return [addReducer, combinedReducer, removeReducer];
+})();
+
+export interface HouxProviderProps {
+  initialReducers: any;
 }
+
 export const HouxProvider: FC<HouxProviderProps> = ({
   children,
-  reducers,
-  logDispatchedActions = false
+  initialReducers
 }) => {
-  const createStore = useCallback(
-    (reducers: ReducerMap, logDispatchedActions: boolean) => {
-      const rootReducer = composeReducers(reducers);
-      const initialState = rootReducer({}, { type: 'STATE_INIT' });
-      const [state, dispatch] = useReducer(rootReducer, initialState);
-      const localDispatch = action => {
-        // enable simple logger
-        if (logDispatchedActions && action.type) {
-          // eslint-disable-next-line no-console
-          console.info(action);
-        }
-        // async actions support
-        if (typeof action === 'function') {
-          return action(localDispatch, state);
-        }
-        return dispatch(action);
-      };
-      return { state, dispatch: localDispatch };
-    },
-    [reducers]
+  Object.entries(initialReducers).forEach(([name, reducer]) => {
+    addReducer({ [name]: reducer });
+  });
+
+  const [state, dispatch] = useReducer(
+    combinedReducer,
+    combinedReducer({}, {})
   );
 
   return (
-    <Context.Provider value={createStore(reducers, logDispatchedActions)}>
+    <StoreContext.Provider
+      value={{ state, dispatch, addReducer, removeReducer }}
+    >
       {children}
-    </Context.Provider>
+    </StoreContext.Provider>
   );
 };
 
-export const useHoux = () => useContext(Context);
+export const useHoux = () => {
+  return useContext(StoreContext);
+};
