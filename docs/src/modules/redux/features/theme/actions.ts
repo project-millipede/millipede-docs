ac44d7478308fa0dafead5cecb1183a8b19eb31d
Gritsch Markus <markus.gritsch@millipede.me>
2020-12-29 14:11:43 +0100

    Remove unused theme feature

--- docs/src/modules/redux/features/theme/actions.ts
+++
@@ -1,9 +0,0 @@
-import { Direction, PaletteMode } from '@material-ui/core';
-import { action } from 'typesafe-actions';
-
-import { CHANGE_THEME } from './actionTypes';
-
-export const changeTheme = (theme: {
-  direction?: Direction;
-  paletteType: PaletteMode;
-}) => action(CHANGE_THEME, theme);
