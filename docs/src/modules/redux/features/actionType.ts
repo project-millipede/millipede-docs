182b73c5cb3d2e1ad2b2a0c3027faea63e4d030b
Gritsch Markus <markus.gritsch@millipede.me>
2020-12-29 14:11:43 +0100

    Remove unused theme feature

--- docs/src/modules/redux/features/actionType.ts
+++
@@ -1,7 +0,0 @@
-import { ActionType } from 'typesafe-actions';
-
-import * as Theme from './theme/actions';
-
-export type ThemeActions = ActionType<typeof Theme>;
-
-export type StoreAction = ThemeActions;
