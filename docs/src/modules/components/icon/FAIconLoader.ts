204e45484e76c4b29621df979204dbad33a89c83
Gritsch Markus <markus.gritsch@millipede.me>
2020-12-29 14:13:44 +0100

    Remove unused code fragments
    - FaIconLoader - migrated
    - vercel.json - not required anymore to generate SSG

--- docs/src/modules/components/icon/FAIconLoader.ts
+++
@@ -1,6 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faRocket, faUserCircle } from '@fortawesome/free-solid-svg-icons';
-
-export const loadFAIcons = () => {
-  library.add(faUserCircle, faRocket);
-};
