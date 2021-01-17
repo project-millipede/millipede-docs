96902c5c8f84380263482e12fd7d8dcdb9d05ef5
gurkerl83 <markus_gritsch@gmx.de>
2021-01-18 00:22:31 +0100

    Refactor team page

--- packages/illustrations/src/components/discover-more/team/Board.tsx
+++ packages/illustrations/src/components/discover-more/team/Board.tsx
@@ -1,10 +1,8 @@
-import { createStyles, makeStyles, Theme } from '@material-ui/core';
-import CardMedia from '@material-ui/core/CardMedia';
+import { Avatar, createStyles, makeStyles, Theme } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import { GitHub, Twitter } from '@material-ui/icons';
+import { GitHub, LinkedIn } from '@material-ui/icons';
 import { Translate } from 'next-translate';
 import useTranslation from 'next-translate/useTranslation';
 import React, { FC } from 'react';
@@ -13,105 +11,86 @@ export const getActiveCoreMembers = (t: Translate) => [
   {
     name: 'Gritsch Markus',
     github: 'gurkerl83',
+    linkedIn: 'markus-gritsch-70952126',
     flag: t('common:founder'),
     city: `${t('common:munich')}, ${t('common:germany')}`
   }
 ];
 
-export const getEmeritiCoreMembers = () => [];
-
-export const getCommunityPartners = () => [];
-
-const useStyles = makeStyles((theme: Theme) =>
+export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    details: {
-      margin: theme.spacing(1, 1, 1, 0)
+    row: {
+      display: 'flex',
+      flexDirection: 'row'
     },
-    cover: {
-      width: theme.spacing(10),
-      height: theme.spacing(10),
-      margin: theme.spacing(2),
-      borderRadius: '50%',
-      flexShrink: 0,
-      backgroundColor: theme.palette.background.default
+    column: {
+      display: 'flex',
+      flexDirection: 'column'
     },
-    icon: {
-      fontSize: 18,
-      padding: theme.spacing(1)
+    avatar: {
+      margin: theme.spacing(2)
     },
-    container: {
-      margin: theme.spacing(2, 0, 4)
+    title: {
+      fontSize: theme.spacing(3),
+      margin: `${theme.spacing(2)} 0`
     }
   })
 );
 
 interface GroupProps {
   title: string;
-  description: string;
   members: Array<any>;
 }
 
-const Group: FC<GroupProps> = ({ title, description, members }) => {
+const Group: FC<GroupProps> = ({ title, members }) => {
   const classes = useStyles();
 
   return (
     <div>
-      <Typography gutterBottom component='h2' variant='h5'>
-        {title}
-      </Typography>
-      <Typography>{description}</Typography>
-
-      {/* className={classes.container} */}
-      <Grid container spacing={2}>
+      <Typography variant='h2'>{title}</Typography>
+      <Grid container>
         {members.map(member => {
-          const { name, flag, city, github, twitter } = member;
+          const { name, flag, city, github, linkedIn } = member;
 
           return (
             <Grid key={name} item xs={12} md={6}>
-              <Paper>
-                <Grid container wrap='nowrap'>
-                  <Grid item>
-                    <CardMedia
-                      className={classes.cover}
-                      image={`https://github.com/${github}.png`}
-                      title='Avatar'
-                    />
-                  </Grid>
-                  <Grid item>
-                    <div className={classes.details}>
-                      <Typography component='h3' variant='h6'>
-                        {name}
-                      </Typography>
-                      <Typography variant='body2' color='textSecondary'>
-                        {flag}
-                      </Typography>
-                      <Typography variant='body2' color='textSecondary'>
-                        {city}
-                      </Typography>
-                      <Grid container>
-                        {github && (
-                          <IconButton
-                            component='a'
-                            href={`https://github.com/${github}`}
-                            className={classes.icon}
-                          >
-                            <GitHub fontSize='inherit' />
-                          </IconButton>
-                        )}
-                        {twitter && (
-                          <IconButton
-                            component='a'
-                            href={`https://twitter.com/${twitter}`}
-                            className={classes.icon}
-                          >
-                            <Twitter fontSize='inherit' />
-                          </IconButton>
-                        )}
-                      </Grid>
-                    </div>
-                  </Grid>
-                </Grid>
-              </Paper>
+              <div className={classes.row}>
+                <div className={classes.column}>
+                  <Avatar
+                    className={classes.avatar}
+                    src={`https://github.com/${github}.png`}
+                  />
+                </div>
+                <div className={classes.column}>
+                  <Typography variant='h6' className={classes.title}>
+                    {name}
+                  </Typography>
+                  <Typography variant='subtitle1' color='textSecondary'>
+                    {flag}
+                  </Typography>
+                  <Typography variant='subtitle1' color='textSecondary'>
+                    {city}
+                  </Typography>
+                  <div className={classes.row}>
+                    {github && (
+                      <IconButton
+                        component='a'
+                        href={`https://github.com/${github}`}
+                      >
+                        <GitHub />
+                      </IconButton>
+                    )}
+                    {linkedIn && (
+                      <IconButton
+                        component='a'
+                        href={`https://www.linkedin.com/in/${linkedIn}`}
+                      >
+                        <LinkedIn />
+                      </IconButton>
+                    )}
+                  </div>
+                </div>
+              </div>
             </Grid>
           );
         })}
@@ -123,22 +102,11 @@ const Group: FC<GroupProps> = ({ title, description, members }) => {
 export const Board: FC = () => {
   const { t } = useTranslation();
   return (
-    <div>
+    <>
       <Group
         title={t('common:active-core-team-title')}
-        description={t('common:active-core-team-description')}
         members={getActiveCoreMembers(t)}
       />
-      <Group
-        title={t('common:emeriti-core-team-title')}
-        description={t('common:emeriti-core-team-description')}
-        members={getEmeritiCoreMembers()}
-      />
-      <Group
-        title={t('common:community-partners-title')}
-        description={t('common:community-partners-description')}
-        members={getCommunityPartners()}
-      />
-    </div>
+    </>
   );
 };
