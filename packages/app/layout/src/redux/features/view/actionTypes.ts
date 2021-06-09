82496aaf68ff7f7e7c1e79cfe50b3a83718cfbb7
Gritsch Markus <markus.gritsch@millipede.me>
2021-06-09 18:13:50 +0200

    Remove unused redux feature

--- packages/app/layout/src/redux/features/view/actionTypes.ts
+++
@@ -1,1 +0,0 @@
-export const HANDLE_DEVICE = 'HANDLE_DEVICE';
--- packages/app/layout/src/redux/features/view/actions.ts
+++
@@ -1,6 +0,0 @@
-import { action } from 'typesafe-actions';
-
-import { HANDLE_DEVICE } from './actionTypes';
-
-export const handleDevice = (isMobile: boolean) =>
-  action(HANDLE_DEVICE, { isMobile });
