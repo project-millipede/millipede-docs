f59aa4503bb18ed227c912bcc514b001c1c65de2
Gritsch Markus <markus.gritsch@millipede.me>
2021-06-09 18:13:50 +0200

    Remove unused redux feature

--- packages/app/layout/src/index.ts
+++ packages/app/layout/src/index.ts
@@ -1,9 +1,3 @@
-export type { ViewActions, StoreAction } from './redux/features/actionType';
-export type { ViewState, RootState } from './redux/features/reducers';
-
-export { reducers } from './redux/features/reducers';
-export { actions } from './redux/features/actionType';
-
 export { Tree } from './components/tree';
 export { AppFrame } from './components/AppFrame';
 export { AppWrapper } from './components/AppWrapper';
