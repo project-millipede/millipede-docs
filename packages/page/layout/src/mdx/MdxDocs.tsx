0893a14ac845f3056717cf025af6005c7c3be539
Gritsch Markus <markus.gritsch@millipede.me>
2021-06-25 00:55:47 +0200

    Replace jss mdxStyles with styled wrapper

--- packages/page/layout/src/mdx/MdxDocs.tsx
+++ packages/page/layout/src/mdx/MdxDocs.tsx
@@ -1,5 +1,6 @@
 import { Components as ComponentUtils } from '@app/render-utils';
 import { PageTypes } from '@app/types';
+import { styled } from '@material-ui/core/styles';
 import { Breadcrumbs, Snackbar } from '@page/components';
 import React, { FC } from 'react';
 
@@ -7,7 +8,6 @@ import { AppContent } from '../components/AppContent';
 import { AppContentFooter } from '../components/AppContentFooter';
 import { AppHead } from '../components/AppHead';
 import { AppTableOfContents } from '../components/AppTableOfContents';
-import { useMdxStyles } from './MDXStyles';
 
 const {
   Media: { Media }
@@ -18,8 +18,55 @@ interface MarkdownDocsProps {
   meta: PageTypes.MetaData;
 }
 
+export const ContentWrapper = styled('div')(({ theme }) => ({
+  fontSize: 16,
+  fontFamily: theme.typography.fontFamily,
+  color: theme.palette.text.primary,
+  '& h1': {
+    fontSize: 48,
+    fontWeight: theme.typography.fontWeightLight,
+    margin: 'auto 0'
+  },
+  '& h2': {
+    fontSize: 40,
+    fontWeight: theme.typography.fontWeightLight
+    // margin: '32px 0 16px'
+  },
+  '& h3': {
+    fontSize: 32,
+    fontWeight: theme.typography.fontWeightLight
+    // margin: '24px 0 16px'
+  },
+  '& h4': {
+    fontSize: 28,
+    fontWeight: theme.typography.fontWeightLight
+    // margin: '24px 0 16px'
+  },
+  '& h5': {
+    fontSize: 24,
+    fontWeight: theme.typography.fontWeightLight
+    // margin: '24px 0 16px'
+  },
+  '& h6': {
+    fontSize: 20,
+    fontWeight: theme.typography.fontWeightLight
+    // margin: '16px 0 16px'
+  },
+  '& blockquote': {
+    borderLeft: '5px solid #ffe564',
+    backgroundColor: 'rgba(255,229,100,0.2)',
+    padding: '4px 24px',
+    margin: '24px 0',
+    '& p': {
+      marginTop: '16px'
+    }
+  },
+  '& img': {
+    maxWidth: '100%'
+  }
+}));
+
 export const MdxDocs: FC<MarkdownDocsProps> = ({ raw, meta, children }) => {
-  const classes = useMdxStyles();
   const { disableToc, ...restMeta } = meta;
 
   return (
@@ -27,16 +74,14 @@ export const MdxDocs: FC<MarkdownDocsProps> = ({ raw, meta, children }) => {
       <AppHead meta={restMeta} />
       {!disableToc && <AppTableOfContents content={raw} />}
       <AppContent disableToc={disableToc}>
-        {children && (
-          <div className={classes.root}>
-            <Media greaterThanOrEqual='md'>
-              <Breadcrumbs />
-            </Media>
-            {children}
-            <Snackbar />
-            <AppContentFooter />
-          </div>
-        )}
+        <ContentWrapper>
+          <Media greaterThanOrEqual='md'>
+            <Breadcrumbs />
+          </Media>
+          {children}
+          <Snackbar />
+          <AppContentFooter />
+        </ContentWrapper>
       </AppContent>
     </>
   );
