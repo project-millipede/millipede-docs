f0369f035f006ef3a8dbbaa7cc608d6c32fdb9aa
Gritsch Markus <markus.gritsch@millipede.me>
2021-06-24 23:22:50 +0200

    Migrate interactive-head from jss to emotion

--- packages/page/layout/src/components/head/InteractiveHead.tsx
+++ packages/page/layout/src/components/head/InteractiveHead.tsx
@@ -1,37 +1,11 @@
-import { SvgIcon, Theme, Typography } from '@material-ui/core';
+import { SvgIcon, Typography } from '@material-ui/core';
 import LinkIcon from '@material-ui/icons/Link';
-import { makeStyles } from '@material-ui/styles';
 import React, { FC, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { useSetRecoilState } from 'recoil';
 
 import { scrollItemsReducer, scrollItemsState } from '../../recoil/features/scroll/page/reducer';
-import { Anchor, CLASS_HEADER } from './InteractiveHead.svc';
-
-export const useStyles = makeStyles((theme: Theme) => ({
-  heading: {
-    '& h2, & h3, & h4': {
-      '& a': {
-        visibility: 'hidden',
-        display: 'inline-block',
-        padding: theme.spacing(0, 1)
-      },
-      '&:hover a': {
-        visibility: 'visible',
-        color: theme.palette.text.secondary,
-        '&:hover': {
-          color: theme.palette.text.primary
-        }
-      }
-    }
-  },
-  headerText: {
-    hyphens: 'auto',
-    '-ms-hyphens': 'auto',
-    '-moz-hyphens': 'auto',
-    '-webkit-hyphens': 'auto'
-  }
-}));
+import { Anchor, CLASS_HEADER, HeaderLink } from './InteractiveHead.svc';
 
 interface InteractiveHeadProps {
   // id generated through slug
@@ -44,8 +18,6 @@ export const InteractiveHead: FC<InteractiveHeadProps> = ({
   variant,
   children
 }) => {
-  const classes = useStyles();
-
   const setScrollItemsState = useSetRecoilState(scrollItemsState);
 
   const addItem = (item: string) => {
@@ -73,7 +45,7 @@ export const InteractiveHead: FC<InteractiveHeadProps> = ({
   }, [inView]);
 
   return (
-    <div className={classes.heading}>
+    <>
       <Anchor id={id} ref={ref} />
 
       <Typography
@@ -89,30 +61,16 @@ export const InteractiveHead: FC<InteractiveHeadProps> = ({
           }}
         >
           {children}
-          {/* <HeaderLink href={'#' + id}>
+          <HeaderLink href={'#' + id}>
             <SvgIcon
               component={LinkIcon}
               fontSize={
                 variant === 'h2' || variant === 'h3' ? 'medium' : 'small'
               }
             />
-          </HeaderLink> */}
-          <a
-            href={'#' + id}
-            style={{
-              display: 'inline-block',
-              textDecoration: 'none'
-            }}
-          >
-            <SvgIcon
-              component={LinkIcon}
-              fontSize={
-                variant === 'h2' || variant === 'h3' ? 'medium' : 'small'
-              }
-            />
-          </a>
+          </HeaderLink>
         </span>
       </Typography>
-    </div>
+    </>
   );
 };
