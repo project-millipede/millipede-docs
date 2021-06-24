81da066a6d8cbbe028e9bf2a2f0c4b5bfa996865
Gritsch Markus <markus.gritsch@millipede.me>
2021-06-24 23:07:50 +0200

    Align breakpoints used for media queries to MUI5
    - remove unused code

--- packages/render-utils/src/components/Responsive.tsx
+++ packages/render-utils/src/components/Responsive.tsx
@@ -1,20 +1,8 @@
 import { useMediaQuery, useTheme } from '@material-ui/core';
 
-const Mobile = ({ children }) => {
-  const theme = useTheme();
-  const tabletMaxWidth = useMediaQuery(theme.breakpoints.down('md'));
-  return tabletMaxWidth ? children : null;
-};
-
-const Desktop = ({ children }) => {
-  const theme = useTheme();
-  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
-  return isDesktop ? children : null;
-};
-
 const isMobile = () => {
   const theme = useTheme();
   return useMediaQuery(theme.breakpoints.down('md'));
 };
 
-export { Mobile, Desktop, isMobile };
+export { isMobile };
