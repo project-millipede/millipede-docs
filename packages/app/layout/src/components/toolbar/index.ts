0c1b40696772b09d5009e417d0aee6d35834401c
Gritsch Markus <markus.gritsch@millipede.me>
2021-06-24 21:58:05 +0200

    Migrate app-toolbar components from jss to emotion

--- packages/app/layout/src/components/toolbar/index.ts
+++ packages/app/layout/src/components/toolbar/index.ts
@@ -1,6 +1,7 @@
-export { SwitchAppToolBar } from './SwitchAppToolBar';
+import { AppBarProps as MuiAppBarProps } from '@material-ui/core/AppBar';
 
-export interface AppToolBarProps {
+export { SwitchAppToolBar } from './SwitchAppToolBar';
+export interface AppBarProps extends MuiAppBarProps {
   isDrawerExpanded?: boolean;
   handleDrawerOpen?: () => void;
 }
