23766d53fad6bd54c8c0f4bcaa236c5ea63d3c9e
Gritsch Markus <markus.gritsch@millipede.me>
2021-06-24 23:34:35 +0200

    Migrate board and publications from jss to emotion

--- packages/illustrations/src/components/discover-more/team/Board.tsx
+++ packages/illustrations/src/components/discover-more/team/Board.tsx
@@ -1,10 +1,10 @@
-import { Avatar, Theme } from '@material-ui/core';
+import { Avatar, Box } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
+import { useTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import { GitHub, LinkedIn } from '@material-ui/icons';
 import AssignmentIcon from '@material-ui/icons/Assignment';
-import { makeStyles } from '@material-ui/styles';
 import { Translate } from 'next-translate';
 import useTranslation from 'next-translate/useTranslation';
 import { useRouter } from 'next/router';
@@ -20,30 +20,13 @@ export const getActiveCoreMembers = (t: Translate) => [
   }
 ];
 
-export const useStyles = makeStyles((theme: Theme) => ({
-  row: {
-    display: 'flex'
-  },
-  column: {
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  avatar: {
-    margin: theme.spacing(2)
-  },
-  title: {
-    fontSize: theme.spacing(3),
-    margin: theme.spacing(2, 0)
-  }
-}));
-
 interface GroupProps {
   title: string;
   members: Array<any>;
 }
 
 const Group: FC<GroupProps> = ({ title, members }) => {
-  const classes = useStyles();
+  const theme = useTheme();
 
   const { push } = useRouter();
 
@@ -60,18 +43,23 @@ const Group: FC<GroupProps> = ({ title, members }) => {
       <Grid container>
         {members.map(member => {
           const { name, flag, city, github, linkedIn } = member;
-
           return (
             <Grid item key={name} xs={12} md={6}>
-              <div className={classes.row}>
-                <div className={classes.column}>
+              <Box sx={{ display: 'flex' }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                   <Avatar
-                    className={classes.avatar}
+                    sx={{ margin: theme.spacing(2) }}
                     src={`https://github.com/${github}.png`}
                   />
-                </div>
-                <div className={classes.column}>
-                  <Typography variant='h6' className={classes.title}>
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <Typography
+                    variant='h6'
+                    sx={{
+                      fontSize: theme.spacing(3),
+                      margin: theme.spacing(2, 0)
+                    }}
+                  >
                     {name}
                   </Typography>
                   <Typography variant='subtitle1' color='textSecondary'>
@@ -80,7 +68,7 @@ const Group: FC<GroupProps> = ({ title, members }) => {
                   <Typography variant='subtitle1' color='textSecondary'>
                     {city}
                   </Typography>
-                  <div className={classes.row}>
+                  <Box sx={{ display: 'flex' }}>
                     {github && (
                       <IconButton
                         component='a'
@@ -105,9 +93,9 @@ const Group: FC<GroupProps> = ({ title, members }) => {
                     >
                       <AssignmentIcon />
                     </IconButton>
-                  </div>
-                </div>
-              </div>
+                  </Box>
+                </Box>
+              </Box>
             </Grid>
           );
         })}
