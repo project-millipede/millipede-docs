06e5e81b4394331a76f878ef9c2ca2ea50da2e7d
Gritsch Markus <markus.gritsch@millipede.me>
2021-06-25 01:38:57 +0200

    Add emotion cache and server
    In the _app component
    - create an initial cache, span cache-provider and serve the cache
    In the _document component
    - create cache, set prepend=true
    - remove material-server stylesheets init, do not enhance app with these stylesheets
    - enhance component, with cache-provider and serve cache
    - extract emotion styles, generate style tags
    - copy style tags to styles array

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,17 @@ import { Portal, Query } from '@app/components';
 import { HouxProvider } from '@app/houx';
 import { AppFrame, AppWrapper } from '@app/layout';
 import { Components } from '@app/render-utils';
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
 import { NextComponentType } from 'next';
 import I18nProvider from 'next-translate/I18nProvider';
 import { AppContext, AppInitialProps, AppProps } from 'next/app';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
 import { AnalyticsProvider } from 'use-analytics';
+
+const cache = createCache({ key: 'css' });
+cache.compat = true;
 
 const {
   Media: { MediaContextProvider }
@@ -26,13 +31,15 @@ const MillipedeApp: NextComponentType<AppContext, AppInitialProps, AppProps> =
                   lang={pageProps && pageProps.__lang}
                   namespaces={pageProps && pageProps.__namespaces}
                 >
-                  <AppWrapper>
-                    <MediaContextProvider>
-                      <AppFrame>
-                        <Component {...pageProps} />
-                      </AppFrame>
-                    </MediaContextProvider>
-                  </AppWrapper>
+                  <CacheProvider value={cache}>
+                    <AppWrapper>
+                      <MediaContextProvider>
+                        <AppFrame>
+                          <Component {...pageProps} />
+                        </AppFrame>
+                      </MediaContextProvider>
+                    </AppWrapper>
+                  </CacheProvider>
                 </I18nProvider>
               </HouxProvider>
             </RecoilRoot>
