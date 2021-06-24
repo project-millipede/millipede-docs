23bbbddeeae8244c3df088bced61eac7ad4ee540
Gritsch Markus <markus.gritsch@millipede.me>
2021-06-24 22:02:02 +0200

    Media query based responsive layouts
    for drawer and app toolbar components

--- packages/app/layout/src/components/drawer/SwitchDrawer.tsx
+++ packages/app/layout/src/components/drawer/SwitchDrawer.tsx
@@ -1,4 +1,9 @@
+import { Link } from '@app/components';
+import { ACTIVE_INDICATOR_WIDTH, TOOLBAR_HEIGHT } from '@app/layout/src/recoil/features/layout/reducer';
 import { Components } from '@app/render-utils';
+import { Divider, IconButton } from '@material-ui/core';
+import { styled } from '@material-ui/core/styles';
+import { ChevronLeft, FirstPage } from '@material-ui/icons';
 import React, { FC } from 'react';
 import { useRecoilValue } from 'recoil';
 
@@ -6,8 +11,24 @@ import { DesktopDrawer, DrawerProps, MobileDrawer } from '.';
 import { navigationState } from '../../recoil/features/pages/reducer';
 import { Tree } from '../tree';
 
+export const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  minHeight: TOOLBAR_HEIGHT,
+  // TODO:
+  // Investigate why a direct height specification causes the component to itch
+  // when tree elements are opened or closed.
+  // height: TOOLBAR_HEIGHT,
+  // Important:
+  // Correct layout shift caused by the border-right style applied to navigation
+  // indicator located in the tree component
+  marginLeft: theme.spacing(2),
+  marginRight: `calc(${theme.spacing(2)} + ${ACTIVE_INDICATOR_WIDTH}px)`
+}));
+
 const {
-  Responsive: { Mobile, Desktop }
+  Media: { Media }
 } = Components;
 
 export const SwitchDrawer: FC<DrawerProps> = ({
@@ -17,35 +38,59 @@ export const SwitchDrawer: FC<DrawerProps> = ({
 }) => {
   const navigation = useRecoilValue(navigationState);
 
-  const { pages, flattenedPages, activePage } = navigation;
+  const { pages, selectedPage, expandedPages } = navigation;
 
   const treeComp = (
     <Tree
       pages={pages}
-      flattenedPages={flattenedPages}
-      activePage={activePage}
+      selectedPage={selectedPage}
+      expandedPages={expandedPages}
     />
+  );
+
+  const headerComp = (
+    <DrawerHeader>
+      <Link
+        href={
+          {
+            pathname: '/'
+          } as any
+        }
+      >
+        <IconButton onClick={handleDrawerClose}>
+          <FirstPage />
+        </IconButton>
+      </Link>
+      <IconButton onClick={handleDrawerClose}>
+        <ChevronLeft />
+      </IconButton>
+    </DrawerHeader>
   );
 
   return (
     <>
-      <Mobile>
+      <Media lessThan='md'>
         <MobileDrawer
+          isDrawerExpanded={isDrawerExpanded}
           handleDrawerOpen={handleDrawerOpen}
           handleDrawerClose={handleDrawerClose}
-          isDrawerExpanded={isDrawerExpanded}
         >
+          {headerComp}
+          <Divider variant='middle' flexItem />
           {treeComp}
         </MobileDrawer>
-      </Mobile>
-      <Desktop>
+      </Media>
+
+      <Media greaterThanOrEqual='md'>
         <DesktopDrawer
-          handleDrawerClose={handleDrawerClose}
           isDrawerExpanded={isDrawerExpanded}
+          handleDrawerClose={handleDrawerClose}
         >
+          {headerComp}
+          <Divider variant='middle' flexItem />
           {treeComp}
         </DesktopDrawer>
-      </Desktop>
+      </Media>
     </>
   );
 };
