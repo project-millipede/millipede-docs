3457385b5f08971dd5cc68963dded89dad87747b
Gritsch Markus <markus.gritsch@millipede.me>
2021-06-27 20:41:36 +0200

    Add component mdx-content
    The component specifies h1 - h6 and block-quote styles which are applied in the mdx content.

--- packages/page/layout/src/mdx/MdxDocs.tsx
+++ packages/page/layout/src/mdx/MdxDocs.tsx
@@ -1,6 +1,5 @@
 import { Components as ComponentUtils } from '@app/render-utils';
 import { PageTypes } from '@app/types';
-import { styled } from '@material-ui/core/styles';
 import { Snackbar } from '@page/components';
 import React, { FC } from 'react';
 
@@ -9,6 +8,7 @@ import { AppContentFooter } from '../components/AppContentFooter';
 import { AppContentHeader } from '../components/AppContentHeader';
 import { AppHead } from '../components/AppHead';
 import { AppTableOfContents } from '../components/AppTableOfContents';
+import { MdxContent } from './MdxContent';
 
 const {
   Media: { Media }
@@ -19,54 +19,6 @@ interface MarkdownDocsProps {
   meta: PageTypes.MetaData;
 }
 
-export const ContentWrapper = styled('div')(({ theme }) => ({
-  fontSize: 16,
-  fontFamily: theme.typography.fontFamily,
-  color: theme.palette.text.primary,
-  '& h1': {
-    fontSize: 48,
-    fontWeight: theme.typography.fontWeightLight,
-    margin: 'auto 0'
-  },
-  '& h2': {
-    fontSize: 40,
-    fontWeight: theme.typography.fontWeightLight
-    // margin: '32px 0 16px'
-  },
-  '& h3': {
-    fontSize: 32,
-    fontWeight: theme.typography.fontWeightLight
-    // margin: '24px 0 16px'
-  },
-  '& h4': {
-    fontSize: 28,
-    fontWeight: theme.typography.fontWeightLight
-    // margin: '24px 0 16px'
-  },
-  '& h5': {
-    fontSize: 24,
-    fontWeight: theme.typography.fontWeightLight
-    // margin: '24px 0 16px'
-  },
-  '& h6': {
-    fontSize: 20,
-    fontWeight: theme.typography.fontWeightLight
-    // margin: '16px 0 16px'
-  },
-  '& blockquote': {
-    borderLeft: '5px solid #ffe564',
-    backgroundColor: 'rgba(255,229,100,0.2)',
-    padding: '4px 24px',
-    margin: '24px 0',
-    '& p': {
-      marginTop: '16px'
-    }
-  },
-  '& img': {
-    maxWidth: '100%'
-  }
-}));
-
 export const MdxDocs: FC<MarkdownDocsProps> = ({ raw, meta, children }) => {
   const { disableToc, ...restMeta } = meta;
 
@@ -75,14 +27,12 @@ export const MdxDocs: FC<MarkdownDocsProps> = ({ raw, meta, children }) => {
       <AppHead meta={restMeta} />
       {!disableToc && <AppTableOfContents content={raw} />}
       <AppContent disableToc={disableToc}>
-        <ContentWrapper>
-          <Media greaterThanOrEqual='md'>
-            <AppContentHeader />
-          </Media>
-          {children}
-          <Snackbar />
-          <AppContentFooter />
-        </ContentWrapper>
+        <Media greaterThanOrEqual='md'>
+          <AppContentHeader />
+        </Media>
+        <MdxContent>{children}</MdxContent>
+        <Snackbar />
+        <AppContentFooter />
       </AppContent>
     </>
   );
