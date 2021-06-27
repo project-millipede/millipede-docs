704b4b6c3b051be03d643c653395ad698090936c
Gritsch Markus <markus.gritsch@millipede.me>
2021-06-27 22:29:44 +0200

    Cleanup styled-tabs component

--- packages/app/components/src/tabs/StyledTabs.tsx
+++ packages/app/components/src/tabs/StyledTabs.tsx
@@ -1,30 +1,20 @@
 import { Tabs as TabsComponent, TabsProps } from '@material-ui/core';
-import { experimentalStyled as styled } from '@material-ui/core/styles';
+import { styled } from '@material-ui/core/styles';
 
 export const StyledTabs = styled(TabsComponent)<TabsProps>(({ theme }) => ({
   '& .MuiTabs-flexContainer': {
     flexWrap: 'wrap'
   },
   '& .MuiTab-root': {
-    // confusing &.Mui compared to & .Mui
     '&.MuiTab-labelIcon': {
       minHeight: theme.spacing(6),
-      // either
       '& .MuiTab-wrapper': {
-        flexDirection: 'row',
-        '& > *:first-child': {
-          marginRight: theme.spacing(1),
-          marginBottom: 0
-        }
+        flexDirection: 'row'
+      },
+      '& svg': {
+        marginBottom: 0,
+        marginRight: theme.spacing(1)
       }
     }
-    // or
-    // '& .MuiTab-wrapper': {
-    //   flexDirection: 'row',
-    //   '& > *:first-child': {
-    //     marginRight: theme.spacing(1),
-    //     marginBottom: 0
-    //   }
-    // }
   }
 }));
