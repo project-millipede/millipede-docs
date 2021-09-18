3acff3884bb7ad9186555a6d26d1e4d75b0f9405
gurkerl83 <markus_gritsch@gmx.de>
2021-09-18 19:18:47 +0200

    Rewrite component toolbar
    Instead of using fresnel, forwarding both versions simultaneously from the server, only a single component gets downloaded and hydrated. The responsive layout gets achieved using plain old media. Refactor hide-on-scroll, render on client exclusively.

--- packages/app/layout/src/components/toolbar/index.ts
+++ packages/app/layout/src/components/toolbar/index.ts
@@ -1,7 +1,1 @@
-import { AppBarProps as MuiAppBarProps } from '@material-ui/core/AppBar';
-
-export { SwitchAppToolBar } from './SwitchAppToolBar';
-export interface AppBarProps extends MuiAppBarProps {
-  isDrawerExpanded?: boolean;
-  handleDrawerOpen?: () => void;
-}
+export { AppBar } from './AppBar';
