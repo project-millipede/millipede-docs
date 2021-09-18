dc645eb9ff059de8cf84322342228900d63af285
gurkerl83 <markus_gritsch@gmx.de>
2021-09-18 19:04:20 +0200

    Rewrite component app-frame
    The application's layout was migrated from flex to grid. The reason for the migration was because a considerable content shift occurred.
    Tests show that the layout grid is unnecessary to fix the content shift; in fact, the flex layout does not introduce any content shift once the refactorings are finished.
    The flex version is still provided; minor adjustments are necessary to replace the grid with the flex-based version.

--- packages/app/layout/src/components/AppFrame.tsx
+++ packages/app/layout/src/components/AppFrame.tsx
@@ -1,12 +1,79 @@
 import { Portal } from '@app/components';
-import { layoutState } from '@app/layout/src/recoil/features/layout/reducer';
+import { AppDrawer } from '@app/layout';
+import {
+  layoutState,
+  TOC_WIDTH
+} from '@app/layout/src/recoil/features/layout/reducer';
+import { NavigationState } from '@app/layout/src/recoil/features/pages/reducer';
+import { styled } from '@material-ui/core/styles';
 import React, { FC, useCallback } from 'react';
 import { useRecoilState } from 'recoil';
 
-import { SwitchDrawer } from './drawer';
-import { SwitchAppToolBar } from './toolbar';
+import { AppBar } from './toolbar';
 
-export const AppFrame: FC = ({ children }) => {
+interface AppGridProps {
+  hasToc: boolean;
+}
+
+export const AppFrameGrid = styled('div', {
+  shouldForwardProp: prop => prop !== 'hasToc'
+})<AppGridProps>(({ theme, hasToc }) => {
+  return {
+    display: 'grid',
+    gridTemplateRows: 'auto',
+    gridTemplateAreas: `'left middle'`,
+    gridTemplateColumns: 'max-content auto',
+    ...(hasToc && {
+      [theme.breakpoints.up('md')]: {
+        gridTemplateAreas: `'left middle right'`,
+        gridTemplateColumns: `max-content auto ${theme.spacing(TOC_WIDTH)}`
+      }
+    })
+  };
+});
+
+export const AppFrameDiv = styled('div')(() => {
+  return {
+    display: 'flex'
+  };
+});
+
+interface AppFrameProps {
+  hasToc?: boolean;
+  navigation?: NavigationState;
+}
+
+// Note:
+// The application's navigation is interactive; the user selects another page (route);
+// this is signaled by an expansion and collapse of the respective navigation group.
+
+// The structure of the pages included in the application is hierarchical; corresponding hierarchy
+// components (tree) are used to enable collapsing and expanding navigation groups.
+
+// The page type used for documentation purposes is dynamic.
+
+// Dynamic page of a reasonable hierarchical level: [...slug].
+// Components for each page: _app with corresponding subcomponents, e.g., app-frame.
+
+// Navigation: Hierarchy components (tree) and corresponding subcomponents
+// Drawer: Integration of navigation
+
+// Remark:
+// In Next, there is no delayed rendering between _app and any page,
+// the components specified in _app are part of any page.
+// Animating navigation groups' collapse and expand behavior requires that components that perform
+// route change animation are always included in _app and appropriate subcomponents, e.g., app-frame;
+// [...slug] must not contain any of these components.
+
+// drawer <= tree
+// + app-frame
+// - [...slug]
+
+export const AppFrame: FC<AppFrameProps> = ({
+  hasToc = false,
+  navigation,
+  children
+}) => {
   const [{ isDrawerExpanded }, setLayout] = useRecoilState(layoutState);
 
   const handleDrawerOpen = useCallback(() => {
@@ -28,21 +95,32 @@ export const AppFrame: FC = ({ children }) => {
   }, []);
 
   return (
-    <div style={{ display: 'flex' }}>
-      <SwitchAppToolBar
+    <>
+      <AppBar
         isDrawerExpanded={isDrawerExpanded}
-        handleDrawerOpen={handleDrawerOpen}
-      />
-
-      <SwitchDrawer
         handleDrawerOpen={handleDrawerOpen}
         handleDrawerClose={handleDrawerClose}
-        isDrawerExpanded={isDrawerExpanded}
       />
 
+      <AppFrameGrid hasToc={hasToc}>
+        <AppDrawer.SwitchDrawer
+          isDrawerExpanded={isDrawerExpanded}
+          navigation={navigation}
+          handleDrawerOpen={handleDrawerOpen}
+          handleDrawerClose={handleDrawerClose}
+        />
+        {children}
+      </AppFrameGrid>
+      {/* <AppFrameDiv>
+        <AppDrawer.SwitchDrawer
+          isDrawerExpanded={isDrawerExpanded}
+          navigation={navigation}
+          handleDrawerOpen={handleDrawerOpen}
+          handleDrawerClose={handleDrawerClose}
+        />
+        {children}
+      </AppFrameDiv> */}
       <Portal.PortalOut portalType={Portal.PortalType.Cursor} />
-
-      {children}
-    </div>
+    </>
   );
 };
