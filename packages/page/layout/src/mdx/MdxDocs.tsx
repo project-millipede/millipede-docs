5764aa0d129791ee7293ea7316ed213d5f0c5e2c
gurkerl83 <markus_gritsch@gmx.de>
2021-09-18 20:11:50 +0200

    Rewrite mdx-docs and mdx-element components
    - The layout of the app was migrated from flex to grid. The component mdx-docs specifies grid-areas for the middle and right grid sections (MDX, TOC), respectively.
    - Remove mdx-content wrapper; docs-based styles are specified within a nested theme provider.

--- packages/page/layout/src/mdx/MdxDocs.tsx
+++ packages/page/layout/src/mdx/MdxDocs.tsx
@@ -1,42 +1,57 @@
+import { DocsThemeProvider } from '@app/layout';
+import { TOC_TOP } from '@app/layout/src/recoil/features/layout/reducer';
+import { NavigationState } from '@app/layout/src/recoil/features/pages/reducer';
 import { Components as ComponentUtils } from '@app/render-utils';
 import { PageTypes } from '@app/types';
+import { Container } from '@material-ui/core';
+import { styled } from '@material-ui/core/styles';
 import { Snackbar } from '@page/components';
-import React, { FC } from 'react';
+import { Toc } from '@stefanprobst/remark-extract-toc';
+import React, { FC, ReactNode } from 'react';
 
-import { AppContent } from '../components/AppContent';
+import { AppTableOfContents } from '../components';
 import { AppContentFooter } from '../components/AppContentFooter';
 import { AppContentHeader } from '../components/AppContentHeader';
-import { AppHead } from '../components/AppHead';
-import { AppTableOfContents } from '../components/AppTableOfContents';
-import { MdxContent } from './MdxContent';
 
-const {
-  Media: { Media },
-  SmoothScroll
-} = ComponentUtils;
-
-interface MarkdownDocsProps {
-  raw: string;
-  meta: PageTypes.MetaData;
+const { SmoothScroll } = ComponentUtils;
+export interface MarkdownDocsProps {
+  disableToc: boolean;
+  slug: Array<string>;
+  toc: Toc;
+  navigation: NavigationState;
+  metaData: PageTypes.MetaData;
+  children: ReactNode;
 }
 
-export const MdxDocs: FC<MarkdownDocsProps> = ({ raw, meta, children }) => {
-  const { disableToc, ...restMeta } = meta;
+export const StyledAppContainer = styled(Container)(({ theme }) => {
+  return {
+    gridArea: 'middle',
+    marginTop: theme.spacing(TOC_TOP)
+  };
+});
 
+export const MdxDocs: FC<MarkdownDocsProps> = ({
+  slug,
+  toc,
+  navigation,
+  metaData,
+  children
+}) => {
   return (
     <>
-      <AppHead meta={restMeta} />
-      {!disableToc && <AppTableOfContents content={raw} />}
-      <AppContent disableToc={disableToc}>
-        <Media greaterThanOrEqual='md'>
-          <AppContentHeader />
-        </Media>
-        <MdxContent>
-          <SmoothScroll>{children}</SmoothScroll>
-        </MdxContent>
-        <Snackbar />
-        <AppContentFooter />
-      </AppContent>
+      <StyledAppContainer>
+        <AppContentHeader
+          slug={slug}
+          navigation={navigation}
+          metaData={metaData}
+        />
+        <SmoothScroll>
+          <DocsThemeProvider>{children}</DocsThemeProvider>
+        </SmoothScroll>
+        <AppContentFooter navigation={navigation} />
+      </StyledAppContainer>
+      <AppTableOfContents toc={toc} sx={{ gridArea: 'right' }} />
+      <Snackbar />
     </>
   );
 };
--- packages/page/layout/src/mdx/index.ts
+++ packages/page/layout/src/mdx/index.ts
@@ -1,2 +1,2 @@
 export { MdxDocs } from './MdxDocs';
-export { h1, h2, h3, h4, h5, h6 } from './MdxElement';
+export { Header, h1, h5, h6, blockquote } from './MdxElement';
