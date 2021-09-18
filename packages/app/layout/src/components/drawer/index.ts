5ae6d450a69aed6f4230ae00f94dc14bebfccd94
gurkerl83 <markus_gritsch@gmx.de>
2021-09-18 20:21:57 +0200

    Refactor drawer components
    - The layout of the app was migrated from flex to grid. The component switch-drawer specifies a grid area for the left grid section.
    - Improve drawer styles

--- packages/app/layout/src/components/drawer/index.ts
+++ packages/app/layout/src/components/drawer/index.ts
@@ -1,8 +1,13 @@
+import { Theme } from '@material-ui/core/styles';
+import { SxProps } from '@material-ui/system';
+
 export { MobileDrawer } from './MobileDrawer';
 export { DesktopDrawer } from './DesktopDrawer';
 export { SwitchDrawer } from './SwitchDrawer';
+
 export interface DrawerProps {
   isDrawerExpanded: boolean;
+  sx: SxProps<Theme>;
   handleDrawerOpen?: () => void;
-  handleDrawerClose: () => void;
+  handleDrawerClose?: () => void;
 }
