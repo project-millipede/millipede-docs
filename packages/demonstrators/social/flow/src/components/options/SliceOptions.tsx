a364dc3c7dee8d73ae39be07665dac81c203df2d
Gritsch Markus <markus.gritsch@millipede.me>
2022-03-06 19:35:28 +0100

    Refactor social demonstrator - part 5
    Slice component, remove unused components

--- packages/demonstrators/social/flow/src/components/options/SliceOptions.tsx
+++ packages/demonstrators/social/flow/src/components/options/SliceOptions.tsx
@@ -1,144 +1,76 @@
-import { APP_CONTENT_HEADER_HEIGHT } from '@app/layout/src/recoil/features/layout/reducer';
-import { scrollStates, ScrollTypes } from '@demonstrators-social/shared';
-import { Close, Share } from '@mui/icons-material';
-import { SpeedDial, SpeedDialAction, SpeedDialIcon } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import { Translate } from 'next-translate';
+import { features } from '@demonstrators-social/shared';
+import {
+  Box,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel
+} from '@mui/material';
 import useTranslation from 'next-translate/useTranslation';
-import React, { FC, useState } from 'react';
-import { SetterOrUpdater, useRecoilState } from 'recoil';
-
-import { Icon } from './Icon';
-
-export interface DockMenuItem {
-  id: string;
-  title: string;
-}
-
-const getSliceOptions = (_t: Translate) => {
-  return [
-    {
-      id: 'header',
-      title: 'Header'
-    },
-    {
-      id: 'media',
-      title: 'media'
-    },
-    {
-      id: 'content',
-      title: 'content'
-    },
-    {
-      id: 'sentiment',
-      title: 'sentiment'
-    },
-    {
-      id: 'comments',
-      title: 'comments'
-    }
-  ];
-};
-
-const createIcons =
-  (
-    hideSpeedDial: () => void,
-    interactionOption: ScrollTypes.Interaction.InteractionOption,
-    setInteractionOption: SetterOrUpdater<ScrollTypes.Interaction.InteractionOption>
-  ) =>
-  ({ id, title }: DockMenuItem) => {
-    return {
-      title,
-      icon: <Icon id={id} />,
-      action: () => {
-        const { activeIds } = interactionOption;
-        const selectedInteractionOption = activeIds[id];
-        setInteractionOption({
-          ...interactionOption,
-          activeIds: {
-            ...interactionOption.activeIds,
-            [id]: !selectedInteractionOption
-          }
-        });
-        hideSpeedDial();
-      }
-    };
-  };
-
-const createButton = ({ title, icon, action }) => (
-  <SpeedDialAction
-    key={title}
-    icon={icon}
-    tooltipTitle={title}
-    onClick={action}
-    tooltipPlacement={'left'}
-  />
-);
-
-const createButtons = (
-  hideSpeedDial: () => void,
-  t: Translate,
-  interactionOption: ScrollTypes.Interaction.InteractionOption,
-  setInteractionOption: SetterOrUpdater<ScrollTypes.Interaction.InteractionOption>
-) => {
-  const buttonsInfo = getSliceOptions(t).map(
-    createIcons(hideSpeedDial, interactionOption, setInteractionOption)
-  );
-
-  return buttonsInfo.map(createButton);
-};
+import React, { FC } from 'react';
+import { useRecoilCallback, useRecoilValue } from 'recoil';
 
 export const SliceOptions: FC = () => {
+  const {
+    scroll: {
+      interaction: {
+        states: { interactionOptionsState }
+      }
+    }
+  } = features;
+
   const { t } = useTranslation();
 
-  const [speedDialOpen, setSpeedDialOpen] = useState(false);
+  const { activeIds } = useRecoilValue(interactionOptionsState);
 
-  const {
-    interaction: { interactionOptionsState }
-  } = scrollStates;
-
-  const [interactionOption, setInteractionOption] = useRecoilState(
-    interactionOptionsState
+  const handleChange = useRecoilCallback(
+    ({ set }) =>
+      (event: React.ChangeEvent<HTMLInputElement>, _checked: boolean) => {
+        set(interactionOptionsState, state => {
+          return {
+            ...state,
+            activeIds: {
+              ...state.activeIds,
+              [event.target.name]: event.target.checked
+            }
+          };
+        });
+      },
+    []
   );
 
-  const handleSpeedDialOpen = () => {
-    setSpeedDialOpen(true);
-  };
-
-  const handleSpeedDialClose = () => {
-    setSpeedDialOpen(false);
-  };
-
-  const handleSpeedDialCloseWithFeedback = () => {
-    setSpeedDialOpen(false);
-  };
-
-  const theme = useTheme();
-
   return (
-    <div
-      style={{
-        width: theme.spacing(APP_CONTENT_HEADER_HEIGHT),
-        height: theme.spacing(APP_CONTENT_HEADER_HEIGHT),
-        margin: theme.spacing(2.75, 2, 2.75),
-        zIndex: 1050
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
       }}
     >
-      <SpeedDial
-        ariaLabel={'select-interaction'}
-        icon={<SpeedDialIcon icon={<Share />} openIcon={<Close />} />}
-        onClose={handleSpeedDialClose}
-        onOpen={handleSpeedDialOpen}
-        open={speedDialOpen}
-        direction='down'
-      >
-        {createButtons(
-          handleSpeedDialCloseWithFeedback,
-          t,
-          interactionOption,
-          setInteractionOption
-        )}
-      </SpeedDial>
-    </div>
+      <FormControl>
+        <FormLabel>
+          {t('pages/pidp/use-case/recognition/index:select-slice-categories')}
+        </FormLabel>
+        <FormGroup row>
+          {Object.entries(activeIds).map(([key, value]) => {
+            return (
+              <FormControlLabel
+                key={`slice-option-${key}`}
+                control={
+                  <Checkbox
+                    size='small'
+                    name={key}
+                    checked={value}
+                    onChange={handleChange}
+                  />
+                }
+                label={t(`pages/pidp/use-case/recognition/index:${key}`)}
+              />
+            );
+          })}
+        </FormGroup>
+      </FormControl>
+    </Box>
   );
 };
---
+++ packages/demonstrators/social/flow/src/components/options/index.ts
@@ -0,0 +1,1 @@
+export { SliceOptions } from './SliceOptions';
