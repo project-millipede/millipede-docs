9e7fb310bb43738fae396d78d1ad85ed6d1524e5
Gritsch Markus <markus.gritsch@millipede.me>
2022-03-06 20:13:13 +0100

    Smaller component adjustments

--- packages/demonstrator/components/src/cursor/shapes/circle.tsx
+++ packages/demonstrator/components/src/cursor/shapes/circle.tsx
@@ -9,26 +9,27 @@ interface Props {
   style?: MotionStyle;
 }
 
-const CircleShape = forwardRef<SVGSVGElement, Props>((props, ref) => {
-  const { radius, fillColor, strokeColor, strokeWidth, style } = props;
-  return (
-    <motion.svg
-      ref={ref}
-      width={`${radius}`}
-      height={`${radius}`}
-      viewBox={`0 0 ${radius} ${radius}`}
-      style={style}
-    >
-      <motion.circle
-        cx={`${radius / 2}`}
-        cy={`${radius / 2}`}
-        r={`${radius / 4}`}
-        fill={fillColor}
-        stroke={strokeColor}
-        strokeWidth={strokeWidth}
-      />
-    </motion.svg>
-  );
-});
+const CircleShape = forwardRef<SVGSVGElement, Props>(
+  ({ radius, fillColor, strokeColor, strokeWidth, style }, ref) => {
+    return (
+      <motion.svg
+        ref={ref}
+        width={`${radius}`}
+        height={`${radius}`}
+        viewBox={`0 0 ${radius} ${radius}`}
+        style={style}
+      >
+        <motion.circle
+          cx={`${radius / 2}`}
+          cy={`${radius / 2}`}
+          r={`${radius / 4}`}
+          fill={fillColor}
+          stroke={strokeColor}
+          strokeWidth={strokeWidth}
+        />
+      </motion.svg>
+    );
+  }
+);
 
 export default CircleShape;
--- packages/demonstrators/social/components/src/input/ChromeInput.tsx
+++ packages/demonstrators/social/components/src/input/ChromeInput.tsx
@@ -1,5 +1,5 @@
-import { INPUT_BORDER_RADIUS, INPUT_HEIGHT } from '@app/layout/src/recoil/features/layout/reducer';
-import { appCompositionState } from '@demonstrator/navigation/src/recoil/features/app/reducers';
+import { INPUT_BORDER_RADIUS, INPUT_HEIGHT } from '@app/layout';
+import { features } from '@demonstrator/navigation';
 import { Fullscreen, FullscreenExit, InfoOutlined, ViewCarousel, ViewColumn } from '@mui/icons-material';
 import { Box, IconButton, InputAdornment, InputBase } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
@@ -10,7 +10,14 @@ import { useRecoilState } from 'recoil';
 interface ChromInputProps {
   fullScreenHandle: FullScreenHandle;
 }
+
 export const ChromeInput: FC<ChromInputProps> = ({ fullScreenHandle }) => {
+  const {
+    app: {
+      states: { appCompositionState }
+    }
+  } = features;
+
   const theme = useTheme();
 
   const [{ isMobile }, setAppComposition] = useRecoilState(appCompositionState);
@@ -23,14 +30,14 @@ export const ChromeInput: FC<ChromInputProps> = ({ fullScreenHandle }) => {
         borderRadius: theme.spacing(INPUT_BORDER_RADIUS)
       }}
       startAdornment={
-        <InputAdornment position='start'>
-          <IconButton>
+        <InputAdornment position='start' sx={{ marginLeft: '8px' }}>
+          <IconButton size='small'>
             <InfoOutlined />
           </IconButton>
         </InputAdornment>
       }
       endAdornment={
-        <InputAdornment position='end' style={{ marginRight: '8px' }}>
+        <InputAdornment position='end' sx={{ marginRight: '8px' }}>
           <Box>
             <IconButton
               size='small'
@@ -58,7 +65,7 @@ export const ChromeInput: FC<ChromInputProps> = ({ fullScreenHandle }) => {
           </Box>
         </InputAdornment>
       }
-      placeholder={'https://bookface.com'}
+      placeholder='https://bookface.com'
     />
   );
 };
