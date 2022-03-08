5541ab088fd97b99d1c927c220bd5063d43cb140
Gritsch Markus <markus.gritsch@millipede.me>
2022-03-08 17:20:58 +0100

    Make play-text use auto-height animation
    Disable play-text in snap-based player sheet for now

--- packages/demonstrator/components/src/player/components/playtext/Playtext.tsx
+++ packages/demonstrator/components/src/player/components/playtext/Playtext.tsx
@@ -1,22 +1,34 @@
-import { StringUtil } from '@app/utils';
-import { Step } from '@demonstrator/components/src/player/types';
+import { Typography } from '@mui/material';
 import useTranslation from 'next-translate/useTranslation';
 import { forwardRef, ForwardRefRenderFunction } from 'react';
 
+import { AnimateHeight } from '../../../animation/components/AnimateHeight';
+import { HeightVariants } from '../../../animation/types';
+import { useStepState } from '../../context/StepProvider';
+import { Step } from '../../types';
+
 interface PlaytextProps {
-  activeStep: Step;
+  steps: Array<Step>;
 }
-const Playtext: ForwardRefRenderFunction<HTMLDivElement, PlaytextProps> = (
-  { activeStep = {} },
-  ref
-) => {
+export const Playtext: ForwardRefRenderFunction<
+  HTMLDivElement,
+  PlaytextProps
+> = ({ steps }, _ref) => {
   const { t } = useTranslation();
+
+  const { target } = useStepState();
+  const activeStep = steps[target] || { description: '' };
 
   const { description } = activeStep;
 
-  return description && !StringUtil.isEmptyString(description) ? (
-    <div ref={ref}>{t(description)}</div>
-  ) : null;
+  return (
+    <AnimateHeight
+      isVisible={activeStep.description != null}
+      variantsType={HeightVariants.Dynamic}
+    >
+      <Typography variant='body1'>{t(description)}</Typography>
+    </AnimateHeight>
+  );
 };
 
 export default forwardRef(Playtext);
