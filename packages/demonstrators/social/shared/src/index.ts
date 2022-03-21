2c04d92c5bf2abdca23dd68fd3b0d37988066cdf
Gritsch Markus <markus.gritsch@millipede.me>
2022-03-22 00:30:14 +0100

    Smaller cleanup

--- packages/demonstrators/social/shared/src/index.ts
+++ packages/demonstrators/social/shared/src/index.ts
@@ -1,5 +1,4 @@
 export * as Scroll from './features/scroll/types';
-export * from './features/timeline/types';
-export * from './features/viewport/types';
+export * as Timeline from './features/timeline/types';
 
 export { features } from './features';
