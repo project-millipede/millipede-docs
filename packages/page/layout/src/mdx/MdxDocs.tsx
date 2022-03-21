bf3fb0e8b1c562a18a2679175daa354f4bebbf3f
Gritsch Markus <markus.gritsch@millipede.me>
2022-03-22 00:13:37 +0100

    Refactor mdx-header components
    Now that loadable components are memoized, re-rendering does not happen for header components anymore; a dedicated matching of a header tag to a custom component is not required anymore.

--- packages/page/layout/src/mdx/MdxDocs.tsx
+++ packages/page/layout/src/mdx/MdxDocs.tsx
@@ -62,7 +62,6 @@ export const MdxDocs: FC<MarkdownDocsProps> = ({
           }}
         </Media>
       )}
-
       <Snackbar />
     </>
   );
--- packages/page/layout/src/mdx/index.ts
+++ packages/page/layout/src/mdx/index.ts
@@ -1,3 +1,6 @@
 export { MdxDocs } from './MdxDocs';
 export { MdxBlog } from './MdxBlog';
-export { h1, h2, h3, h4, h5, h6, blockquote } from './MdxElement';
+export { h1, h5, h6, blockquote } from './MdxElement';
+export {
+  default as Header // HeaderProps
+} from './MdxHeader';
