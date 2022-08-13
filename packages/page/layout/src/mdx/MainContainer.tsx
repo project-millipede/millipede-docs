211a8eca7a70f9ed112d7da50dfa85fd93be8be1
Gritsch Markus <markus.gritsch@millipede.me>
2022-08-13 15:32:00 +0200

    Onboard main-container component

---
+++ packages/page/layout/src/mdx/MainContainer.tsx
@@ -0,0 +1,65 @@
+import { TOC_TOP } from '@app/layout';
+import { Breakpoint, Container, SxProps, Theme } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { domAnimation, LazyMotion, m, Variants } from 'framer-motion';
+import { FC } from 'react';
+
+export const StyledMainContainer = styled(Container)(({ theme }) => {
+  return {
+    gridArea: 'app-center',
+    marginTop: theme.spacing(TOC_TOP)
+  };
+}) as typeof Container;
+
+interface MainContainerProps {
+  id: string;
+  sx?: SxProps<Theme>;
+  maxWidth?: false | Breakpoint;
+}
+
+export const MainContainer: FC<MainContainerProps> = ({
+  id,
+  sx,
+  maxWidth,
+  children
+}) => {
+  return (
+    <StyledMainContainer component='div' id={id} sx={sx} maxWidth={maxWidth}>
+      {children}
+    </StyledMainContainer>
+  );
+};
+
+/**
+ * Note:
+ * The m-component can be used in exactly the same way as motion, but it comes with no features preloaded.
+ * These are then provided by LazyMotion.
+ */
+export const MotionArticle = m.article;
+
+export const fade: Variants = {
+  initial: { opacity: 0 },
+  enter: {
+    opacity: 1,
+    transition: { type: 'tween', duration: 0.4 }
+  },
+  exit: {
+    opacity: 0,
+    transition: { type: 'tween', duration: 0.4 }
+  }
+};
+
+export const Article: FC = ({ children }) => {
+  return (
+    <LazyMotion features={domAnimation}>
+      <MotionArticle
+        initial='initial'
+        animate='enter'
+        exit='exit'
+        variants={fade}
+      >
+        {children}
+      </MotionArticle>
+    </LazyMotion>
+  );
+};
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,24 +1,31 @@
 import { HiddenUnderlineLink } from '@app/components';
-import { BlogFrame, BlogThemeProvider, TOC_TOP } from '@app/layout';
+import { BlogFrame, BlogThemeProvider } from '@app/layout';
+import { Navigation, PageTypes } from '@app/types';
 import { Blurb, Post, Posts } from '@blog/components';
-import { Chip, Container, Divider, Typography } from '@mui/material';
+import { Chip, Divider, Typography } from '@mui/material';
 import { grey } from '@mui/material/colors';
 import { styled } from '@mui/material/styles';
+import { Components, Mdx } from '@page/layout';
 import { compareDesc } from 'date-fns';
 import { GetStaticProps } from 'next';
 import { mergeProps } from 'next-merge-props';
-import { ReactElement } from 'react';
+import { FC, ReactElement } from 'react';
 
 import { GetStaticContentProps, getStaticContentPropsBlogIndex } from '../../docs/src/lib/getStaticContentProps';
 import { GetStaticNavigationProps } from '../../docs/src/lib/getStaticNavigationProps';
 import { GetStaticTranslationProps } from '../../docs/src/lib/getStaticTranslationProps';
 import { NextPageWithLayout } from '../../docs/src/lib/types';
 
-export const StyledAppContainer = styled(Container)(({ theme }) => ({
-  gridArea: 'app-center',
-  marginTop: theme.spacing(TOC_TOP),
-  maxWidth: '80ch'
-})) as typeof Container;
+const { AppHead } = Components;
+const { MainContainer } = Mdx;
+
+const metaDataBlogLanding: PageTypes.MetaData = {
+  title: "Untether, for good - Let's fuse pixels of arbitrary apps",
+  description:
+    'An interactive blog probing building blocks of futuristic privacy',
+  keywords: 'Blog, Privacy enhancing technology',
+  author: 'Markus Gritsch'
+};
 
 export const Header = styled('header')(({ theme }) => ({
   display: 'flex',
@@ -27,58 +34,70 @@ export const Header = styled('header')(({ theme }) => ({
   marginBottom: theme.spacing(2)
 }));
 
+const BlogHeader: FC = () => {
+  return (
+    <Header>
+      <Typography variant='h1'>Untether, for good</Typography>
+      <Typography variant='h2'>Let's fuse pixels of arbitrary apps</Typography>
+      <Blurb variant='h3'>
+        An interactive blog probing building blocks of futuristic privacy, by
+        <Typography
+          component={HiddenUnderlineLink}
+          variant='h3'
+          sx={{
+            fontFamily: "'Roboto Mono', monospace",
+            fontWeight: 500,
+            '&:hover': {
+              color: grey[600]
+            }
+          }}
+          href={{
+            pathname: '/docs/[...slug]',
+            query: { slug: 'discover-more/team'.split('/') }
+          }}
+          prefetch={false}
+        >
+          {` Markus Gritsch`}
+        </Typography>
+      </Blurb>
+    </Header>
+  );
+};
+
 export type DynamicPageBlogProps = GetStaticTranslationProps &
   GetStaticContentProps &
   GetStaticNavigationProps;
 
 const Blog: NextPageWithLayout<DynamicPageBlogProps> = ({ content }) => {
   return (
-    <StyledAppContainer id='blog-landing' component='main' maxWidth={false}>
-      <Header>
-        <Typography variant='h1'>Untether, for good</Typography>
-        <Typography variant='h2'>
-          Let's fuse pixels of arbitrary apps
-        </Typography>
-        <Blurb variant='h3'>
-          An interactive blog probing building blocks of futuristic privacy, by
-          <Typography
-            variant='h3'
-            sx={{
-              fontFamily: "'Roboto Mono', monospace",
-              fontWeight: 500,
-              '&:hover': {
-                color: grey[600]
-              }
-            }}
-            component={HiddenUnderlineLink}
-            href={{
-              pathname: '/docs/[...slug]',
-              query: { slug: 'discover-more/team'.split('/') }
-            }}
-            prefetch={false}
-          >
-            {` Markus Gritsch`}
-          </Typography>
-        </Blurb>
-      </Header>
-
-      <Divider>
-        <Chip label='Posts' />
-      </Divider>
-      <Posts>
-        {Array.isArray(content) &&
-          content
-            .sort((a, b) =>
-              compareDesc(
-                new Date(a.metaData.editedAt),
-                new Date(b.metaData.editedAt)
+    <>
+      <AppHead metaData={metaDataBlogLanding} />
+      <MainContainer
+        id='blog-landing'
+        maxWidth={false}
+        sx={{
+          maxWidth: '80ch'
+        }}
+      >
+        <BlogHeader />
+        <Divider>
+          <Chip label='Posts' />
+        </Divider>
+        <Posts>
+          {Array.isArray(content) &&
+            content
+              .sort((a, b) =>
+                compareDesc(
+                  new Date(a.metaData.editedAt),
+                  new Date(b.metaData.editedAt)
+                )
               )
-            )
-            .map((post, index) => {
-              return <Post key={`post-${index}`} post={post as any} />;
-            })}
-      </Posts>
-    </StyledAppContainer>
+              .map((post, index) => {
+                return <Post key={`post-${index}`} post={post as any} />;
+              })}
+        </Posts>
+      </MainContainer>
+    </>
   );
 };
 
@@ -93,7 +112,7 @@ export const getStaticProps: GetStaticProps = mergeProps(
   }
 );
 
-Blog.getLayout = (page: ReactElement) => {
+Blog.getLayout = (page: ReactElement, _navigation: Navigation) => {
   return (
     <BlogThemeProvider>
       <BlogFrame>{page}</BlogFrame>
