ee9ce2eebb445d43a2838ea4b303c7a93dc7ed88
Gritsch Markus <markus.gritsch@millipede.me>
2022-08-13 15:02:49 +0200

    Simplify custom next link component further
    Refactor hooks smooth-scroll and reset-scroll

--- packages/app/layout/src/hooks/use-reset-scroll-on-route-change.ts
+++ packages/app/layout/src/hooks/use-reset-scroll-on-route-change.ts
@@ -1,11 +1,17 @@
+import { MittEmitter } from '@app/render-utils';
 import { features } from '@page/layout';
-import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useResetRecoilState } from 'recoil';
 
-export const useResetScrollOnRouteChange = () => {
-  const { events } = useRouter();
+/**
+ * Note:
+ * Do not use the NextJs hook "use-router" in this custom hook; constant re-renders get executed;
+ * unfortunately, there are some bugs in the implementation.
+ *
+ * Instead, the NextJs router events object from the _app component is given as an argument.
+ */
 
+export const useResetScrollOnRouteChange = (events: MittEmitter) => {
   const {
     scroll: {
       states: { scrollState }
@@ -17,15 +23,13 @@ export const useResetScrollOnRouteChange = () => {
   useEffect(() => {
     resetScrollState();
 
-    const handleRouteChange = () => {
-      resetScrollState();
-    };
+    const handleRouteChangeStart = () => resetScrollState();
 
-    events.on('routeChangeStart', handleRouteChange);
+    events.on('routeChangeStart', handleRouteChangeStart);
 
-    // If the component is unmounted, unsubscribe from the event with the `off` method:
+    // If the component unmounts, unsubscribe from the event with the 'off' method.
     return () => {
-      events.off('routeChangeStart', handleRouteChange);
+      events.off('routeChangeStart', handleRouteChangeStart);
     };
   }, []);
 };
