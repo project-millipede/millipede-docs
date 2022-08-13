335ef65332df45425cf1f7e6ea6c99303f176651
Gritsch Markus <markus.gritsch@millipede.me>
2022-08-13 14:34:56 +0200

    Onboard statement component

--- packages/app/components/src/index.ts
+++ packages/app/components/src/index.ts
@@ -2,6 +2,7 @@ export { CustomIcon, getIconByName, loadFAIcons } from './icon';
 export * from './link';
 export * as Tabs from './tabs';
 export * as Box from './box';
+export * as Statement from './statement';
 export { default as Portal } from './portal';
 export { default as Stepper } from './stepper';
 export * as Pdf from './pdf';
